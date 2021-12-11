ece41970ae68b34f70daa104d75cef44b9bf63de
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 19:54:33 -0800

    init About.js

---
+++ src/components/Home/About.js
@@ -0,0 +1,40 @@
+const About = () => {
+  return (
+    <div className='about'>
+      <p>
+        My name is Brian E. Nguyen and I graduated from California State
+        University, Long Beach in May 2020 with a bachelor's degree in computer
+        science. I am currently seeking an internship or an entry-level position
+        in full-stack development.
+      </p>
+      <p>
+        From the moment I have written my first script in Python, I knew that
+        software development is the career choice for me. It has allowed me to
+        challenge myself and to improve on my skills, not just in programming,
+        but also on creativity and team-working. I hope to produce high-quality
+        and beautiful websites that will be used and appreciated by many.
+      </p>
+      <p>
+        I am currently working at Code Ninjas, Cerritos as a coding instructor.
+        Working here combines two of my passions: programming and working with
+        kids. I tutor students with Scratch and JavaScript to help them build
+        video games. Also, I have developed and led my own coding camp that
+        received 100% positive feedback from students.
+      </p>
+      <p>
+        I formerly served as the president of the Tzu Chi Collegiate Association
+        at CSULB, a collegiate chapter of its mother organization, the Tzu Chi
+        Foundation. This volunteering organization focuses on humanitarian aid,
+        medicine, education, and more. I have devoted over 300 hours of my time
+        serving this organization and it has been a life-changing experience for
+        me. Various volunteering activities include recording videos for Tzu
+        Chi's news outlet, packing food at food distributions, tutoring children
+        at tutoring centers, and helping at medical outreaches. As an alumni, I
+        continue to volunteer for the Tzu Chi Foundation and to support the
+        CSULB chapter.
+      </p>
+    </div>
+  );
+};
+
+export default About;
