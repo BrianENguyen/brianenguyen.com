858e45a9ca514bb68b828a7c9004e6ab116644a8
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 19:53:03 -0800

    init Quote.js

---
+++ src/components/Home/Quote.js
@@ -0,0 +1,14 @@
+const Quote = () => {
+  return (
+    <div className='quote'>
+      <p>
+        <em>
+          A fulfilling life is not preoccupied with material objects, prestige,
+          or power. It is a life that is filled with true friendships, sharing,
+          and caring for each other.
+        </em>
+      </p>
+      <p>Dharma Master Cheng Yen</p>
+    </div>
+  );
+};
