7576a7547832fd62ff336ecbfd742f85d3779a48
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-11 13:23:51 -0800

    Navbar: button always appears on reload

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './Navbar.css';
 
@@ -32,7 +32,9 @@ const Navbar = () => {
       <span className='navbar__toggle-btn--bar'></span>
     </div>
   );
-
+  useEffect(() => {
+    showMobileButton();
+  }, []);
   window.addEventListener('resize', showMobileButton);
   window.addEventListener('scroll', showBackground);
 
