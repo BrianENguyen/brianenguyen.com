9c25f1ddb2ccf2a512004c6833945a2d1328a209
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 20:26:12 -0800

    HomeView: import Skillset and Tools

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,15 @@
 import Quote from '../components/Home/Quote';
 import About from '../components/Home/About';
+import Skillset from '../components/Home/Skillset';
+import Tools from '../components/Home/Tools';
 
 const HomeView = () => {
   return (
     <div className='home'>
       <Quote />
       <About />
+      <Skillset />
+      <Tools />
     </div>
   );
 };
