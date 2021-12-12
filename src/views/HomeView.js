ee87185156a22c09916a8f84bcf27627fa3a846e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 14:48:38 -0800

    HomeView: container

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,15 +1,21 @@
+import { Container } from '@mui/material';
+
 import Quote from '../components/Home/Quote';
 import Passion from '../components/Home/Passion';
 import Skillset from '../components/Home/Skillset';
 import Tools from '../components/Home/Tools';
+import WebCardList from '../components/Home/WebCardList';
 
 const HomeView = () => {
   return (
     <div className='home'>
-      <Quote />
-      <Passion />
-      <Skillset />
-      <Tools />
+      <Container>
+        <Quote />
+        <Passion />
+        <Skillset />
+        <Tools />
+        <WebCardList />
+      </Container>
     </div>
   );
 };
