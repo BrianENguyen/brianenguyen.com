2938d84bf9f010199bc72e0cdcf791078ff6c6aa
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 12:47:27 -0800

    Passion: basic markup

--- src/components/Home/Passion.js
+++ src/components/Home/Passion.js
@@ -1,24 +1,24 @@
+import { Grid, Container } from '@mui/material';
+
 const Passion = () => {
   return (
     <div className='passion'>
-      <p>
-        My name is Brian E. Nguyen and I graduated from California State
-        University, Long Beach in May 2020 with a bachelor's degree in computer
-        science. I am currently seeking an internship or an entry-level position
-        in front-end development.
-      </p>
-      <p>
-        From the moment I have written my first script in Python, I knew that
-        computer science is the career choice for me. It has allowed me to
-        challenge myself and to improve on my skills, not just in programming,
-        but also on creativity and team-working. I hope to produce high-quality
-        and beautiful websites that will be used and appreciated by many.
-      </p>
-      <p>
-        In addition to my passion in web development, I am a coding instructor
-        at Code Ninjas Cerritos and Fullerton, and an active volunteer for the
-        Tzu Chi Foundation
-      </p>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            *INSERT IMAGE*
+          </Grid>
+          <Grid item xs={6}>
+            <h2>My passion is front-end web development</h2>
+            <p>
+              I love making websites look beautiful. I have years of experience
+              being a web developer with many different technologies, such as
+              HTML/CSS/JS, Vue, React, and more. If you need a website made,
+              then feel free to reach out to me
+            </p>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
