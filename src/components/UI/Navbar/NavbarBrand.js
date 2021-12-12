232c8c17503fd83c8211d6905a3398222f260a2e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 12:14:32 -0800

    Navbar: fixed padding and styles

--- src/components/UI/Navbar/NavbarBrand.js
+++ src/components/UI/Navbar/NavbarBrand.js
@@ -2,7 +2,7 @@ const NavbarBrand = () => {
   return (
     <img
       src='https://res.cloudinary.com/buraiyen/image/upload/v1620240495/BEN_Website/BEN-logo-transparent_rintta.png'
-      width='100px'
+      width='150px'
       alt='BEN'
     ></img>
   );
