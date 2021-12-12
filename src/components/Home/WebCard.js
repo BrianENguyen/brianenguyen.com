ba1995a6f423fcfdb78f44d195e36a604b0cfb07
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 14:16:35 -0800

    Webcard: props and style fixes

--- src/components/Home/WebCard.js
+++ src/components/Home/WebCard.js
@@ -8,17 +8,10 @@ const WebCard = (props) => {
   return (
     <Card className='webcard'>
       <CardContent>
-        <h1>Test</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged
-        </p>
-        <Link className='webcard__link' to='/contact'>
-          Test
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <Link className='webcard__link' to={props.link}>
+          {props.linkText}
         </Link>
       </CardContent>
     </Card>
