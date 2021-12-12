16903f5929983ac36f2cb646fe7a09bfd0a550ae
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 14:47:18 -0800

    WebCardList: more info

--- src/components/Home/WebCardList.js
+++ src/components/Home/WebCardList.js
@@ -4,20 +4,29 @@ import WebCard from './WebCard';
 const WebCardList = () => {
   const cardContent = [
     {
+      title: 'About',
+      description:
+        'Get to know more about me, the technologies I use, and my future goals',
+      linkText: 'About me',
+      link: '/portofilio',
+    },
+    {
       title: 'Portfolio',
-      description: 'Feel free to take a look at projects that I have done',
+      description: 'Feel free to take a look at projects that I have worked on',
       linkText: 'View portfolio',
       link: '/portofilio',
     },
     {
       title: 'Resume',
-      description: 'View my resume',
-      linkText: 'View resume',
+      description:
+        'View my resume to know more about my education, work experience, and more',
+      linkText: 'View resume ',
       link: '/resume',
     },
     {
-      title: 'Portfolio',
-      description: 'Feel free to take a look at projects that I have done',
+      title: 'Contact',
+      description:
+        "Get in touch we me for any reason, whether it'd be for services, inquiries or just chatting",
       linkText: 'View portfolio',
       link: '/portofilio',
     },
