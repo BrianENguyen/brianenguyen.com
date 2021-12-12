d12f6cf39ebdada9ea5b7d99516c4e2ce4ecf46a
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 12:03:42 -0800

    init NavbarBrand

---
+++ src/components/UI/Navbar/NavbarBrand.js
@@ -0,0 +1,11 @@
+const NavbarBrand = () => {
+  return (
+    <img
+      src='https://res.cloudinary.com/buraiyen/image/upload/v1620240495/BEN_Website/BEN-logo-transparent_rintta.png'
+      width='100px'
+      alt='BEN'
+    ></img>
+  );
+};
+
+export default NavbarBrand;
