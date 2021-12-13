589d43570a5b5210dc0d7a5fc7f5991683fcae67
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 16:29:35 -0800

    moved Quote to AboutView

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,9 +1,12 @@
+import Quote from '../components/About/Quote';
 import Backstory from '../components/About/Backstory';
 import Skillset from '../components/About/Skillset';
+
 const AboutView = () => {
   return (
     <div className='about'>
       <h1>About</h1>
+      <Quote />
       <Backstory />
       <Skillset />
     </div>
