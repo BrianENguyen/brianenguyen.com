c2677ead3a8d76280feb9baaadd1874a6c6699e7
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 21:27:38 -0800

    Skillset: initial icons

--- src/components/About/Skillset.js
+++ src/components/About/Skillset.js
@@ -1,7 +1,51 @@
+import { Grid } from '@mui/material';
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiVuedotjs,
+  SiVuetify,
+  SiReact,
+  SiMaterialui,
+  SiBootstrap,
+  SiExpress,
+} from 'react-icons/si';
+
+import './Skillset.css';
+
 const Skillset = () => {
   return (
     <div className='skillset'>
       <h1>Skillset</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={2} md={4}>
+          <SiHtml5 className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiCss3 className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiJavascript className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiVuedotjs className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiVuetify className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiReact className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiMaterialui className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiBootstrap className='tech-item' />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <SiExpress className='tech-item' />
+        </Grid>
+      </Grid>
     </div>
   );
 };
