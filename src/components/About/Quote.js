e43837d39588ab4c4f2338a939dcdb4f34e0f97d
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 20:19:50 -0800

    Quote: image

--- src/components/About/Quote.js
+++ src/components/About/Quote.js
@@ -1,14 +1,24 @@
+import { Grid } from '@mui/material';
+
 const Quote = () => {
   return (
     <div className='quote'>
-      <p>
-        <em>
-          A fulfilling life is not preoccupied with material objects, prestige,
-          or power. It is a life that is filled with true friendships, sharing,
-          and caring for each other.
-        </em>
-      </p>
-      <p>Dharma Master Cheng Yen</p>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <img
+            src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1087/v1639368996/BEN_Website/BEN_rosegarden.jpg'
+            width='500px'
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <p>
+            A fulfilling life is not preoccupied with material objects,
+            prestige, or power. It is a life that is filled with true
+            friendships, sharing, and caring for each other.
+          </p>
+          <p>Dharma Master Chen Yeng</p>
+        </Grid>
+      </Grid>
     </div>
   );
 };
