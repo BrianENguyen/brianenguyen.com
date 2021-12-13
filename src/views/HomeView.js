7b00d04aa9290d5dbf038cb7fa26af58d4477ff8
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 16:56:04 -0800

    Jumbotron: testing props

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,10 +3,16 @@ import { Container } from '@mui/material';
 import Passion from '../components/Home/Passion';
 import RecentProjects from '../components/Home/RecentProjects';
 import WebCardList from '../components/Home/WebCardList';
+import Jumbotron from '../components/UI/Jumbotron';
 
 const HomeView = () => {
+  const jumboImage =
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1481/v1620240501/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg';
+  const jumboTitle = 'Frontend Web Developer';
+
   return (
     <div className='home'>
+      <Jumbotron image={jumboImage} title={jumboTitle} />
       <Container>
         <Passion />
         <WebCardList />
