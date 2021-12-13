97dd6d8314ba4516b6ecb0ba3cc84668f6caad8d
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 17:25:57 -0800

    PortfolioView: jumbo

--- src/views/PortfolioView.js
+++ src/views/PortfolioView.js
@@ -1,5 +1,15 @@
+import Jumbotron from '../components/UI/Jumbotron';
+
 const PortfolioView = () => {
-  return <h1>Portfolio</h1>;
+  const jumboText = 'Portfolio';
+  const jumboImage =
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240500/BEN_Website/jumbotron/BEN_code_hijtco.jpg';
+  return (
+    <div className='portfolio'>
+      <Jumbotron image={jumboImage} title={jumboText} />
+      <h1>Portfolio</h1>
+    </div>
+  );
 };
 
 export default PortfolioView;
