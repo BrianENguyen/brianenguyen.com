19ab33c5f06eae19e280bb3160661d80d101d4ce
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-12 16:25:50 -0800

    moved Skillset.js; import to AboutView

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,9 +1,11 @@
 import Backstory from '../components/About/Backstory';
+import Skillset from '../components/About/Skillset';
 const AboutView = () => {
   return (
     <div className='about'>
       <h1>About</h1>
       <Backstory />
+      <Skillset />
     </div>
   );
 };
