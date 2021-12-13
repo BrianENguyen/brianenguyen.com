5ea6f402d301c064f8d8c1e44d59e193666566de
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-13 11:35:12 -0800

    Skillset: styles

--- src/components/About/Skillset.js
+++ src/components/About/Skillset.js
@@ -23,46 +23,55 @@ const Skillset = () => {
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiHtml5 className='tech-item' />
+              <div className='tech-item__title '>HTML5</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiCss3 className='tech-item' />
+              <div className='tech-item__title '>CSS3</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiJavascript className='tech-item' />
+              <div className='tech-item__title '>JavaScript</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiVuedotjs className='tech-item' />
+              <div className='tech-item__title '>Vue.js</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiVuetify className='tech-item' />
+              <div className='tech-item__title '>Vuetify</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiReact className='tech-item' />
+              <div className='tech-item__title '>React.js</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiMaterialui className='tech-item' />
+              <div className='tech-item__title '>Material User Interface</div>
             </Card>
           </Grid>
           <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiBootstrap className='tech-item' />
+              <div className='tech-item__title '>Bootstrap</div>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
+          <Grid item xs={6} sm={6} md={4}>
             <Card className='tech-item__card'>
               <SiExpress className='tech-item' />
+              <div className='tech-item__title '>Express.js</div>
             </Card>
           </Grid>
         </Grid>
