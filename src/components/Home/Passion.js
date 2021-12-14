8b89299a7aa8255a4e1eaec96452ebf5c1647e2e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 15:27:04 -0800

    Passion: responsive styles

--- src/components/Home/Passion.js
+++ src/components/Home/Passion.js
@@ -7,20 +7,22 @@ const Passion = () => {
     <div className='passion'>
       <Container>
         <Grid container spacing={2}>
-          <Grid item sm={12} md={6}>
+          <Grid item sm={12} md={5}>
             <img
               src='https://res.cloudinary.com/buraiyen/image/upload/v1620240502/BEN_Website/BEN_profile_pic_x81uf8.jpg'
               className='passion__image'
             />
           </Grid>
-          <Grid item sm={12} md={6}>
-            <h2>My passion is front-end web development</h2>
-            <p>
-              I love making websites look beautiful. I have years of experience
-              being a web developer with many different technologies, such as
-              HTML/CSS/JS, Vue, React, and more. If you need a website made,
-              then feel free to reach out to me
-            </p>
+          <Grid item sm={12} md={7}>
+            <div className='passion__text'>
+              <h2>My passion is front-end web development</h2>
+              <p>
+                I love making websites look beautiful. I have years of
+                experience being a web developer with many different
+                technologies, such as HTML/CSS/JS, Vue, React, and more. If you
+                need a website made, then feel free to reach out to me
+              </p>
+            </div>
           </Grid>
         </Grid>
       </Container>
