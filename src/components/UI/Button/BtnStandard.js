fcc490f81df603c50231151f9aad3cf9a3a04a2b
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 15:57:08 -0800

    Buttons: style and hover

--- src/components/UI/Button/BtnStandard.js
+++ src/components/UI/Button/BtnStandard.js
@@ -1,5 +1,7 @@
+import './Buttons.css';
+
 const BtnStandard = (props) => {
-  return <button>{props.children}</button>;
+  return <button className='btn btn-standard'>{props.children}</button>;
 };
 
 export default BtnStandard;
