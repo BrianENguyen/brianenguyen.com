f4ec8b73d19a32a0955a14214723e85488a2ddd9
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 13:12:53 -0800

    RecentProjects: loop

--- src/components/Home/RecentProjects.js
+++ src/components/Home/RecentProjects.js
@@ -1,7 +1,11 @@
 import RecentProject from './RecentProject';
-
+import { Grid } from '@mui/material';
 const RecentProjects = () => {
   const recentProjects = [
+    {
+      title: 'CSULB Tzu Ching',
+      src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1443/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
+    },
     {
       title: 'CSULB Tzu Ching',
       src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1443/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
@@ -11,9 +15,13 @@ const RecentProjects = () => {
   return (
     <div className='recent-projects'>
       <h1>Recent Projects</h1>
-      {recentProjects.map((project, i) => (
-        <RecentProject key={i} title={project.title} src={project.src} />
-      ))}
+      <Grid container spacing={2}>
+        {recentProjects.map((project, i) => (
+          <Grid item xs={6} md={6}>
+            <RecentProject key={i} title={project.title} src={project.src} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
