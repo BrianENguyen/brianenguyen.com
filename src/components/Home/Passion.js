29d7595065354f4e48d8ef9bf46423cd9f0fe4ac
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 14:53:28 -0800

    Passion: image and styles

--- src/components/Home/Passion.js
+++ src/components/Home/Passion.js
@@ -1,14 +1,19 @@
 import { Grid, Container } from '@mui/material';
 
+import './Passion.css';
+
 const Passion = () => {
   return (
     <div className='passion'>
       <Container>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
-            *INSERT IMAGE*
+          <Grid item sm={12} md={6}>
+            <img
+              src='https://res.cloudinary.com/buraiyen/image/upload/v1620240502/BEN_Website/BEN_profile_pic_x81uf8.jpg'
+              className='passion__image'
+            />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item sm={12} md={6}>
             <h2>My passion is front-end web development</h2>
             <p>
               I love making websites look beautiful. I have years of experience
