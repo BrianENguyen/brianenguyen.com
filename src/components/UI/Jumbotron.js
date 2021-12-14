b4b3ca36d9832c9587c8f9e8a7950c2f77a5f4fc
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 12:23:03 -0800

    Jumbotron: fixed some spacing

--- src/components/UI/Jumbotron.js
+++ src/components/UI/Jumbotron.js
@@ -3,8 +3,9 @@ import './Jumbotron.css';
 const Jumbotron = (props) => {
   const logo = (
     <img
+      className='jumbotron__logo'
       src='https://res.cloudinary.com/buraiyen/image/upload/v1620240495/BEN_Website/BEN-logo-transparent_rintta.png'
-      width='300px'
+      alt=''
     />
   );
 
@@ -15,8 +16,10 @@ const Jumbotron = (props) => {
         backgroundImage: `url(${props.image})`,
       }}
     >
-      {props.logo && logo}
-      <h1 className='jumbotron__title'>{props.title}</h1>
+      <div className='jumbotron__content'>
+        {props.logo && logo}
+        <h1 className='jumbotron__title '>{props.title}</h1>
+      </div>
     </div>
   );
 };
