817ea1d7690d78d4ae3ba3a3c4cbfa3d9bcfd551
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 21:27:17 -0800

    ThuView: content

--- src/views/project/ThuView.js
+++ src/views/project/ThuView.js
@@ -1,3 +1,5 @@
+import { Container } from '@mui/material';
+
 import Jumbotron from '../../components/UI/Jumbotron';
 
 const ThuView = () => {
@@ -6,6 +8,28 @@ const ThuView = () => {
   return (
     <div className='thu-website'>
       <Jumbotron title={jumboTitle} />
+      <Container maxWidth='xl'>
+        <p>
+          Thu Nguyen is currently a college student studying at Cypress College.
+          She plans to transfer to Califonia State University, Fullerton to
+          pursue a Bachelor's in Accounting. This website is made to get to know
+          more about her, view her resume, and get in contact with her
+        </p>
+        <h2>Tech Stack</h2>
+        <ul>
+          <li>React</li>
+          <li>Material UI</li>
+          <li>React Router</li>
+        </ul>
+        <img
+          src='https://res.cloudinary.com/buraiyen/image/upload/v1639545745/BEN_Website/projects/thuhnguyen_cov4jp.png'
+          width='1000px'
+        />
+        <img
+          src='https://res.cloudinary.com/buraiyen/image/upload/v1639545839/BEN_Website/projects/thuhnguyenresume_xmwppk.png'
+          width='1000px'
+        />
+      </Container>
     </div>
   );
 };
