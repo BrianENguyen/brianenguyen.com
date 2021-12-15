b5ec4da45da07eda651a9f935d0423b37eaebfa9
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-14 21:07:28 -0800

    init ThuView

---
+++ src/views/project/ThuView.js
@@ -0,0 +1,13 @@
+import Jumbotron from '../../components/UI/Jumbotron';
+
+const ThuView = () => {
+  const jumboTitle = "Thu Nguyen's Website";
+
+  return (
+    <div className='thu-website'>
+      <Jumbotron title={jumboTitle} />
+    </div>
+  );
+};
+
+export default ThuView;
