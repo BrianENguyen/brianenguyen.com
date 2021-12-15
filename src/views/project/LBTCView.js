484adc0266b825d832f4e1df4d5c2ae833414bfe
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-15 13:12:25 -0800

    LBTCView: info and tech stack

--- src/views/project/LBTCView.js
+++ src/views/project/LBTCView.js
@@ -10,6 +10,28 @@ const LBTCView = () => {
   return (
     <div>
       <Jumbotron title={jumboTitle} />
+      <Container maxWidth='md' className='thu-website'>
+        <p className='project-view__description'>
+          <span className='project-view__description__title'>
+            CSULB Tzu Ching
+          </span>{' '}
+          is a volunteering organization at Califonia State University, Long
+          Beach that focuses on charity, medicine, education, and humanistic
+          culture. It is the collegiate chapter of its mother organization: The
+          Tzu Chi Foundation. This website showcases information about the
+          organization, recent and upcoming volunteering events, board members,
+          and photos
+        </p>
+        <div className='project-view__tech-stack'>
+          <h2>Tech Stack</h2>
+          <ul>
+            <li>Vue.js</li>
+            <li>Vuetify</li>
+            <li>Animate.css</li>
+            <li>Animate on Scroll (AOS)</li>
+          </ul>
+        </div>
+      </Container>
     </div>
   );
 };
