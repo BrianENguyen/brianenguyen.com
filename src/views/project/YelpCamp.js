45097dd61e8fc9d95f11d00dc8863b7e0fbd7185
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 12:08:52 -0800

    YelpCamp: desc and tech stack

--- src/views/project/YelpCamp.js
+++ src/views/project/YelpCamp.js
@@ -1,3 +1,5 @@
+import { Container } from '@mui/material';
+
 import Jumbotron from '../../components/UI/Jumbotron';
 import BtnStandard from '../../components/UI/Button/BtnStandard';
 import './ProjectView.css';
@@ -8,6 +10,23 @@ const YelpCamp = () => {
   return (
     <div className='yelpcamp'>
       <Jumbotron title={jumboTitle} />
+      <Container maxWidth='md'>
+        <p className='project-view__description'>
+          <span className='project-view__description__title'>YelpCamp</span>,
+          inspired by <em>Yelp</em>, is a user-friendly web-application that
+          allows users to create and view various camping places from all over
+          the world and to write reviews on them
+        </p>
+        <div className='project-view__tech-stack'>
+          <h2>Tech Stack</h2>
+          <ul>
+            <li>Embedded JavaScript / CSS / JavaScript</li>
+            <li>Bootstrap 5</li>
+            <li>MongoDB</li>
+            <li>Express.js</li>
+          </ul>
+        </div>
+      </Container>
     </div>
   );
 };
