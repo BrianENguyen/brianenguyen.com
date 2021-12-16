426bb19c25d1577c7c4189dc92ffe03a471ed9c9
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 13:11:54 -0800

    FirstSiteView: description

--- src/views/project/FirstSiteView.js
+++ src/views/project/FirstSiteView.js
@@ -28,6 +28,44 @@ const FirstSiteView = () => {
           </ul>
         </div>
       </Container>
+
+      <Container>
+        <div className='project-view__personal-notes'>
+          <h2>Personal Notes</h2>
+          <p>
+            Creating this website was more than two years in the making. I
+            started developing this back in 2019 with the concepts that I have
+            learned from Colt Steele's Web Dev Bootcamp. The reason why this
+            project took longer to complete was that I was eager to create my
+            website midway through the course, so I used concepts that I already
+            knew; however, I didn't know enough concepts to develop it, such as
+            routing, file organization, components, and app deployment. This led
+            me to become unmotivated to complete it sooner than I expected. I
+            also had to take some time off from the boot camp to focus on my
+            studies at CSULB.
+          </p>
+          <p>
+            After I graduated in 2020, I wanted to finish the boot camp to gain
+            more knowledge on web development. It was a difficult and
+            time-consuming journey, but it was all worth it in the end. I took
+            the new concepts that I've learned from that boot camp and
+            completely reworked my website.
+          </p>
+          <p>
+            I decided to retire this version of my website for many reasons. The
+            biggest one is how slow this website performs without paid hosting.
+            Since I did use traditional HTML files to markup my website, sending
+            requests to the server every time when going to different pages will
+            cause the website slow down. Switching to a front-end framework
+            using the single-page-application architecture will not only make my
+            website faster, but it will also cost me less money
+          </p>
+          <p>
+            Other reasons for retiring this website include changing the design
+            and removing some unnecessary content, like the Photography section
+          </p>
+        </div>
+      </Container>
     </div>
   );
 };
