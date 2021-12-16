77b5f786e6fb7dad3c422e36daf2e114475bba33
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 12:39:00 -0800

    ErrorView: jumbo content

--- src/views/ErrorView.js
+++ src/views/ErrorView.js
@@ -1,10 +1,18 @@
 import Jumbotron from '../components/UI/Jumbotron';
 
 const ErrorView = () => {
+  const jumboImage =
+    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg';
   const jumboTitle = '404 Error';
+  const jumboDescription =
+    'The page that you are on does not or no longer exists. If this is an issue, then feel free to contact me';
   return (
     <div className='error'>
-      <Jumbotron title={jumboTitle} />
+      <Jumbotron
+        image={jumboImage}
+        title={jumboTitle}
+        description={jumboDescription}
+      />
     </div>
   );
 };
