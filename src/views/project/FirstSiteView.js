482af89ed49bae8b7eef9551af8b148b66043af4
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 12:56:48 -0800

    FirstSiteView: desc and tech stack

--- src/views/project/FirstSiteView.js
+++ src/views/project/FirstSiteView.js
@@ -5,11 +5,29 @@ import BtnStandard from '../../components/UI/Button/BtnStandard';
 import './ProjectView.css';
 
 const FirstSiteView = () => {
-  const jumboTitle = 'My Old Personal Website';
+  const jumboTitle = 'My First Personal Website';
 
   return (
     <div className='old-site'>
       <Jumbotron title={jumboTitle} />
+      <Container maxWidth='md'>
+        <p className='project-view__description'>
+          <span className='project-view__description__title'>
+            My first personal website
+          </span>{' '}
+          is my first attempt at developing, designing, and hosting a website
+          online. It features the same sections that you see on this website
+          (minus Photography)
+        </p>
+        <div className='project-view__tech-stack'>
+          <h2>Tech Stack</h2>
+          <ul>
+            <li>Embedded JavaScript / CSS</li>
+            <li>Bootstrap 4</li>
+            <li>Express.js</li>
+          </ul>
+        </div>
+      </Container>
     </div>
   );
 };
