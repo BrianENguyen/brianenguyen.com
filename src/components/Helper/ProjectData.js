2331ace2a14ced730b6f0039aa75162536cfe291
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 13:30:14 -0800

    init ProjectData.js

---
+++ src/components/Helper/ProjectData.js
@@ -0,0 +1,26 @@
+const Projects = [
+  {
+    title: "Thu Nguyen's Website",
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1723/v1639518576/BEN_Website/recent_projects/thu.jpg',
+    link: '/thu-nguyen',
+  },
+  {
+    title: 'CSULB Tzu Ching',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1443/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
+    link: '/csulb-tzu-ching',
+  },
+  {
+    title: 'YelpCamp',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1639529477/BEN_Website/projects/hugues-de-buyer_camp_fc2lzb.jpg',
+    link: '/yelpcamp',
+  },
+  {
+    title: 'First Personal Website',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1236/v1639689762/BEN_Website/projects/ben_firstsite_cover.jpg',
+    link: '/first-personal-site',
+  },
+  {
+    title: 'Kraken Bracket',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1639530141/BEN_Website/projects/florian-olivo_gaming_im9ugb.jpg',
+  },
+];
