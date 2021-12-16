cf626d3db2dd8ddfa3b071424594db904bd84ab5
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-16 12:46:29 -0800

    init OldSiteView

---
+++ src/views/project/OldSiteView.js
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+import Jumbotron from '../../components/UI/Jumbotron';
+import BtnStandard from '../../components/UI/Button/BtnStandard';
+import './ProjectView.css';
+
+const OldSiteView = () => {
+  const jumboTitle = 'My Old Personal Website';
+
+  return <div className='old-site'></div>;
+};
+
+export default OldSiteView;
