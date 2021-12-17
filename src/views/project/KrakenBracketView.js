1f4f6287dee8d557fd78f5b55eba89741e3d16b7
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-17 11:29:58 -0800

    KrakenBracket: notes

--- src/views/project/KrakenBracketView.js
+++ src/views/project/KrakenBracketView.js
@@ -55,7 +55,44 @@ const KrakenBracketView = () => {
         />
       </Container>
       <Container>
-        <div className='project-view__personal-notes'></div>
+        <div className='project-view__personal-notes'>
+          <h2>Personal Notes:</h2>
+          <p>
+            Working on this project with my other five group members made me
+            feel like I was working for a software development company, and it
+            was all thanks to our professor. He simulated the class so that it
+            would be the same as one would expect from working in a software
+            development position; this includes documents (business
+            requirements, project plans, sprint backlogs, etc.), core
+            requirements, unit & integration tests, scope & resource creep, and
+            front-end views
+          </p>
+          <p>
+            My group and I spent countless hours to dedicate to our senior
+            project. We met many times each week, both weekdays and weekends, at
+            school to work on our tasks. We would even spend 12 hours straight
+            on a Saturday to work together. It was definitely a stressful
+            experience and there are many times that we wanted to give up. I
+            would wake up every morning, panicking that I don't have a task done
+            or that I might fail this class, because that was how hard it was.
+          </p>
+          <p>
+            The coronavirus hit our class hard and everything had to shift
+            online. Although meeting with my group members and attending class
+            were easier, they both had a different dynamic and they were not as
+            engaging as meeting in person. Also, we were promised funds from
+            Google to deploy our application, but we never received them. These
+            hard times led to our project to be incomplete when the semester
+            ended.
+          </p>
+          <p>
+            Overall, I am forever grateful for taking this class with the
+            professor that I had. Although we experienced a lot of stress and
+            hardships, I have learned what it's like to be a software developer.
+            I will take everything that I have learned in this class to heart
+            and will apply my knowldege in my future positions.
+          </p>
+        </div>
       </Container>
     </div>
   );
