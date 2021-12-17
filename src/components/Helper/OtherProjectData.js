5f3fbdd7f8e3e6d1ef5ad170006b0a37bd86079e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-17 10:53:58 -0800

    init OldProjectData and import

---
+++ src/components/Helper/OtherProjectData.js
@@ -0,0 +1,7 @@
+const OtherProjectData = [
+  { title: 'SIMD Instructions', link: '/' },
+  { title: 'Probability and Stats', link: '/' },
+  { title: 'Dittoshare', link: '/' },
+];
+
+export default OtherProjectData;
