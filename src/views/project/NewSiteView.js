06e7026dd541ddff646ab441a1f6b4b8771d2237
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-17 13:24:59 -0800

    NewSiteView: desc, tech stack, button

--- src/views/project/NewSiteView.js
+++ src/views/project/NewSiteView.js
@@ -12,20 +12,31 @@ const NewSiteView = () => {
       <Jumbotron title={jumboTitle} />
       <Container maxWidth='md'>
         <p className='project-view__description'>
-          <span className='project-view__description__title'></span>{' '}
+          <span className='project-view__description__title'>
+            My new website
+          </span>{' '}
+          is a redesigned and redeveloped version from my old one that was made
+          back in 2020. This website has improved speed and improved UI / UX
         </p>
         <div className='project-view__tech-stack'>
           <h2>Tech Stack</h2>
           <ul>
-            <li></li>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Material UI</li>
           </ul>
         </div>
-      </Container>
 
-      <Container>
-        <div className='project-view__personal-notes'>
-          <h2>Personal Notes</h2>
-          <p></p>
+        <div>
+          <p>
+            <a
+              href='https://github.com/Brian-E-Nguyen/BEN-Website-React'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <BtnStandard>View Repository</BtnStandard>
+            </a>
+          </p>
         </div>
       </Container>
     </div>
