f93442e13c93cdc361fcf891bba85add9d1ca246
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-17 10:01:42 -0800

    import WebCardData

--- src/components/Home/WebCardList.js
+++ src/components/Home/WebCardList.js
@@ -1,41 +1,12 @@
 import { Grid } from '@mui/material';
 
+import WebCardData from '../Helper/WebCardData';
 import WebCard from './WebCard';
 const WebCardList = () => {
-  const cardContent = [
-    {
-      title: 'About',
-      description:
-        'Get to know more about me, the technologies I use, and my future goals',
-      linkText: 'About me',
-      link: '/about',
-    },
-    {
-      title: 'Portfolio',
-      description: 'Feel free to take a look at projects that I have worked on',
-      linkText: 'View portfolio',
-      link: '/portfolio',
-    },
-    {
-      title: 'Resume',
-      description:
-        'View my resume to know more about my education, work experience, and more',
-      linkText: 'View resume ',
-      link: '/resume',
-    },
-    {
-      title: 'Contact',
-      description:
-        "Get in touch we me for any reason, whether it'd be for services, inquiries or just chatting",
-      linkText: 'View portfolio',
-      link: '/contact',
-    },
-  ];
-
   return (
     <div>
       <Grid container spacing={3}>
-        {cardContent.map((content, i) => (
+        {WebCardData.map((content, i) => (
           <Grid item xs={12} md={4}>
             <WebCard
               title={content.title}
