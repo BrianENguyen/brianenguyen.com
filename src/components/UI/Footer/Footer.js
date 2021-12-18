d72331381aa227fae07b804f5eb74a3d21fb37d1
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 12:18:48 -0800

    Footer: black color; blocks

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -8,29 +8,30 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <Container maxWidth='xl'>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={4}>
-            <div className='footer__section'>
-              <a
-                href='mailto:brian.edison.nguyen@gmail.ccom'
-                className='footer__link'
-              >
-                brian.edison.nguyen@gmail.com
-              </a>
-            </div>
-          </Grid>{' '}
-          <Grid item xs={12} md={4}>
-            {/* FIXME: misaligned brand */}
-            <FooterBrand />
-          </Grid>{' '}
-          <Grid item xs={12} md={4}>
-            <div className='footer__section'>
-              <FooterButtons />
-            </div>
+        <div className='footer__block'>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={4}>
+              <div className='footer__section'>
+                <a
+                  href='mailto:brian.edison.nguyen@gmail.ccom'
+                  className='footer__link'
+                >
+                  brian.edison.nguyen@gmail.com
+                </a>
+              </div>
+            </Grid>{' '}
+            <Grid item xs={12} md={4}>
+              <FooterBrand />
+            </Grid>{' '}
+            <Grid item xs={12} md={4}>
+              <div className='footer__section'>
+                <FooterButtons />
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </Container>
-      <hr />
+      <hr className='footer__line-divider' />
       <Container maxWidth='xs'>
         <p>{new Date().getFullYear()} Brian Nguyen. All rights reserved</p>
       </Container>
