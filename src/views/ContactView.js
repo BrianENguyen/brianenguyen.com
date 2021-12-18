f923beca581506199ba85b29ebb7c5e17d3a48af
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 13:25:47 -0800

    ContactView: basic structure

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -1,13 +1,22 @@
 import Jumbotron from '../components/UI/Jumbotron';
+import { Container } from '@mui/material';
 
 const ContactView = () => {
   const jumboText = 'Contact';
   const jumboImage =
     'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240502/BEN_Website/jumbotron/BEN_casino_gwmqob.jpg';
   return (
-    <div className='portfolio'>
+    <div className='contact'>
       <Jumbotron image={jumboImage} title={jumboText} />
-      <h1>Contact</h1>
+      <Container maxWidth='lg'>
+        <p>
+          You can contact me directly at{' '}
+          <a href='mailto:brian.edison.nguyen@gmail.com'>
+            brian.edison.nguyen@gmail.com
+          </a>
+          , or feel free to use the form below to email me
+        </p>
+      </Container>
     </div>
   );
 };
