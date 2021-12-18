f9898884515017621b6d2fa9d10a517987dabc1b
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 12:32:28 -0800

    FooterButtons: instagram and size change

--- src/components/UI/Footer/FooterButtons.js
+++ src/components/UI/Footer/FooterButtons.js
@@ -1,21 +1,28 @@
 import { IconButton } from '@mui/material';
-import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { SiGithub, SiInstagram } from 'react-icons/si';
+import { GrLinkedinOption } from 'react-icons/gr';
 import './Footer.css';
 
 const FooterButtons = () => {
   const github = 'https://github.com/Brian-E-Nguyen/';
   const linkedin = 'https://www.linkedin.com/in/brian-edison-nguyen/';
+  const instagram = 'https://www.instagram.com/b_rye_the_asian_spy/';
 
   return (
     <div className='footer_buttons'>
       <a href={github} target='_blank' rel='noreferrer'>
         <IconButton component='span' className='footer__button'>
-          <SiGithub />
+          <SiGithub className='footer__button__image' />
         </IconButton>
       </a>
       <a href={linkedin} target='_blank' rel='noreferrer'>
         <IconButton component='span' className='footer__button'>
-          <SiLinkedin />
+          <GrLinkedinOption className='footer__button__image' />
+        </IconButton>
+      </a>
+      <a href={instagram} target='_blank' rel='noreferrer'>
+        <IconButton component='span' className='footer__button'>
+          <SiInstagram className='footer__button__image' />
         </IconButton>
       </a>
     </div>
