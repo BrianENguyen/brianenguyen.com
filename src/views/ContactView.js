772ddbe0760e48260f723a98b638229c19ae8d99
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 14:55:40 -0800

    ContactView: white bg

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -1,12 +1,17 @@
-import Jumbotron from '../components/UI/Jumbotron';
 import { Container } from '@mui/material';
+
+import Jumbotron from '../components/UI/Jumbotron';
+import ContactForm from '../components/Contact/ContactForm';
 
 const ContactView = () => {
   const jumboText = 'Contact';
   const jumboImage =
     'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240502/BEN_Website/jumbotron/BEN_casino_gwmqob.jpg';
   return (
-    <div className='contact'>
+    <div
+      className='contact'
+      style={{ backgroundColor: 'white', color: 'black' }}
+    >
       <Jumbotron image={jumboImage} title={jumboText} />
       <Container maxWidth='lg'>
         <p>
@@ -16,6 +21,7 @@ const ContactView = () => {
           </a>
           , or feel free to use the form below to email me
         </p>
+        <ContactForm />
       </Container>
     </div>
   );
