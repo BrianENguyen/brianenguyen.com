3670e520e9fbd093d4b4e0dc9fb5bbb6b564c131
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 14:55:17 -0800

    ContactForm: inputs and temp style

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,47 @@
-const ContactForm = () => {
-  return <form className='contact-form'></form>;
+import { TextField, Grid } from '@mui/material';
+
+import BtnStandard from '../UI/Button/BtnStandard';
+import './ContactForm.css';
+
+const ContactForm = (props) => {
+  return (
+    <form className='contact-form'>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={6}>
+          <TextField
+            className='contact-form__field'
+            required
+            id='outlined-required'
+            label='First Name'
+          />
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <TextField
+            className='contact-form__field'
+            required
+            id='outlined-required'
+            label='Last Name'
+          />
+        </Grid>
+      </Grid>
+
+      <TextField
+        className='contact-form__field'
+        required
+        id='outlined-required'
+        label='Email'
+      />
+      <TextField
+        className='contact-form__field'
+        required
+        multiline
+        maxRows={4}
+        id='outlined-required'
+        label='Your Message'
+      />
+      <BtnStandard />
+    </form>
+  );
 };
 
 export default ContactForm;
