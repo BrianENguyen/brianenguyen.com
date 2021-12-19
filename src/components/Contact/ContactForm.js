3397a4aaa38d2a32c40a1e05999689aa827e0158
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-18 18:11:14 -0800

    ContactForm: basic logic

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,29 +1,46 @@
 import { TextField, Grid } from '@mui/material';
 import emailjs from 'emailjs-com';
-import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+import { formSchema } from './ContactFormValidation';
 import BtnStandardLight from '../UI/Button/BtnStandardLight';
 import './ContactForm.css';
 
 const ContactForm = (props) => {
-  const sendEmail = (data, e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        'service_1xwndxg',
-        'template_fx6y7uj',
-        e.target,
-        'user_4vVpxSkvODPvEMWOc492H'
-      )
-      .then((result) => {
-        console.log(result.text);
-      })
-      .catch((error) => {
-        console.log(error.text);
-      });
-    e.target.reset();
+  const sendEmail = async (event) => {
+    event.preventDefault();
 
-    // console.log(data);
+    const fName = document.getElementById('first-name').value;
+    const lName = document.getElementById('last-name').value;
+    const e = document.getElementById('email').value;
+    const m = document.getElementById('message').value;
+
+    let formData = {
+      firstName: fName,
+      lastName: lName,
+      email: e,
+      message: m,
+    };
+    const isValid = await formSchema.isValid(formData);
+    if (isValid) {
+      emailjs
+        .sendForm(
+          'service_1xwndxg',
+          'template_fx6y7uj',
+          event.target,
+          'user_4vVpxSkvODPvEMWOc492H'
+        )
+        .then((result) => {
+          console.log(result.text);
+        })
+        .catch((error) => {
+          console.log(error.text);
+        });
+      event.target.reset();
+      alert('You submitted the form!');
+    } else {
+      alert('Uh oh!');
+    }
   };
 
   return (
@@ -32,7 +49,7 @@ const ContactForm = (props) => {
         <Grid item xs={6} md={6}>
           <TextField
             className='contact-form__field'
-            id='outlined-required'
+            id='first-name'
             label='First Name'
             name='first_name'
             placeholder='First name'
@@ -41,7 +58,7 @@ const ContactForm = (props) => {
         <Grid item xs={6} md={6}>
           <TextField
             className='contact-form__field'
-            id='outlined-required'
+            id='last-name'
             label='Last Name'
             name='last_name'
           />
@@ -50,7 +67,7 @@ const ContactForm = (props) => {
 
       <TextField
         className='contact-form__field'
-        id='outlined-required'
+        id='email'
         label='Email'
         name='email'
         type='email'
@@ -60,10 +77,11 @@ const ContactForm = (props) => {
         className='contact-form__field'
         multiline
         maxRows={4}
-        id='outlined-required'
+        id='message'
         label='Your Message'
         name='message'
       />
+      <BtnStandardLight>Submit</BtnStandardLight>
     </form>
   );
 };
