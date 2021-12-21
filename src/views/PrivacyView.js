ca1aa567500fd434c3fbee206fa1a8ab14226f69
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-21 13:35:06 -0800

    PrivacyLog

--- src/views/PrivacyView.js
+++ src/views/PrivacyView.js
@@ -3,6 +3,7 @@ import '../components/PrivacyPolicy/PrivacyPolicy.css';
 
 import PrivacyIntro from '../components/PrivacyPolicy/PrivacyIntro';
 import PrivacyInformation from '../components/PrivacyPolicy/PrivacyInformation';
+import PrivacyLog from '../components/PrivacyPolicy/PrivacyLog';
 
 const PrivacyView = () => {
   return (
@@ -10,21 +11,7 @@ const PrivacyView = () => {
       <Container>
         <PrivacyIntro />
         <PrivacyInformation />
-        <div>
-          <h2>Log Files</h2>
-          <p>
-            The Site follows a standard procedure of using log files. These
-            files log visitors when they visit websites. All hosting companies
-            do this and a part of hosting services' analytics. The information
-            collected by log files include internet protocol (IP) addresses,
-            browser type, Internet Service Provider (ISP), date and time stamp,
-            referring/exit pages, and possibly the number of clicks. These are
-            not linked to any information that is personally identifiable. The
-            purpose of the information is for analyzing trends, administering
-            the site, tracking users' movement on the website, and gathering
-            demographic information.
-          </p>
-        </div>
+        <PrivacyLog />
 
         <div class='privacy-description'>
           <h2>Cookies and Web Beacons</h2>
