59ebf675c78214e3c9ca3934b294169f6a110edc
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-21 13:43:28 -0800

    PrivacyThirdParty

--- src/components/PrivacyPolicy/PrivacyThirdParty.js
+++ src/components/PrivacyPolicy/PrivacyThirdParty.js
@@ -1,21 +1,38 @@
 const PrivacyThirdParty = () => {
   return (
     <div>
-      <h2>Cookies and Web Beacons</h2>
+      <h2>Advertising Partners Privacy Policies</h2>
       <p>
-        Like any other website, the Site uses 'cookies'. These cookies are used
-        to store information including visitors' preferences, and the pages on
-        the website that the visitor accessed or visited. The information is
-        used to optimize the users' experience by customizing our web page
-        content based on visitors' browser type and/or other information.
+        You may consult this list to find the Privacy Policy for each of the
+        advertising partners of the Site.
+      </p>
+      <p>
+        Third-party ad servers or ad networks uses technologies like cookies,
+        JavaScript, or Web Beacons that are used in their respective
+        advertisements and links that appear on the Site, which are sent
+        directly to users' browser. They automatically receive your IP address
+        when this occurs. These technologies are used to measure the
+        effectiveness of their advertising campaigns and/or to personalize the
+        advertising content that you see on websites that you visit.
+      </p>
+      <p>
+        Note that the Site has no access to or control over these cookies that
+        are used by third-party advertisers.
       </p>
 
+      <h2>Third Party Privacy Policies</h2>
       <p>
-        For more general information on cookies, please read{' '}
-        <a href='https://www.privacypolicyonline.com/what-are-cookies/'>
-          "What Are Cookies" from Cookie Consent
-        </a>
-        .
+        The Site's Privacy Policy does not apply to other advertisers or
+        websites. Thus, we are advising you to consult the respective Privacy
+        Policies of these third-party ad servers for more detailed information.
+        It may include their practices and instructions about how to opt-out of
+        certain options.{' '}
+      </p>
+      <p>
+        You can choose to disable cookies through your individual browser
+        options. To know more detailed information about cookie management with
+        specific web browsers, it can be found at the browsers' respective
+        websites.
       </p>
     </div>
   );
--- src/views/PrivacyView.js
+++ src/views/PrivacyView.js
@@ -5,6 +5,7 @@ import PrivacyIntro from '../components/PrivacyPolicy/PrivacyIntro';
 import PrivacyInformation from '../components/PrivacyPolicy/PrivacyInformation';
 import PrivacyLog from '../components/PrivacyPolicy/PrivacyLog';
 import PrivacyCookies from '../components/PrivacyPolicy/PrivacyCookies';
+import PrivacyThirdParty from '../components/PrivacyPolicy/PrivacyThirdParty';
 
 const PrivacyView = () => {
   return (
@@ -14,50 +15,7 @@ const PrivacyView = () => {
         <PrivacyInformation />
         <PrivacyLog />
         <PrivacyCookies />
-
-        <div class='privacy-description'></div>
-        <div>
-          <h2>Advertising Partners Privacy Policies</h2>
-          <p>
-            You may consult this list to find the Privacy Policy for each of the
-            advertising partners of the Site.
-          </p>
-
-          <p>
-            Third-party ad servers or ad networks uses technologies like
-            cookies, JavaScript, or Web Beacons that are used in their
-            respective advertisements and links that appear on the Site, which
-            are sent directly to users' browser. They automatically receive your
-            IP address when this occurs. These technologies are used to measure
-            the effectiveness of their advertising campaigns and/or to
-            personalize the advertising content that you see on websites that
-            you visit.
-          </p>
-
-          <p>
-            Note that the Site has no access to or control over these cookies
-            that are used by third-party advertisers.
-          </p>
-        </div>
-        <div>
-          <h2>Third Party Privacy Policies</h2>
-
-          <p>
-            The Site's Privacy Policy does not apply to other advertisers or
-            websites. Thus, we are advising you to consult the respective
-            Privacy Policies of these third-party ad servers for more detailed
-            information. It may include their practices and instructions about
-            how to opt-out of certain options.{' '}
-          </p>
-
-          <p>
-            You can choose to disable cookies through your individual browser
-            options. To know more detailed information about cookie management
-            with specific web browsers, it can be found at the browsers'
-            respective websites.
-          </p>
-        </div>
-
+        <PrivacyThirdParty />
         <div>
           <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
           <p>
