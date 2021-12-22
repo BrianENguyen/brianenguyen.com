1dd345e5d3eadea847f40e28abe2fdda519521e7
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-22 11:47:34 -0800

    PrivacyDialog: removed header and styles

--- src/components/PrivacyPolicy/PrivacyDialog.js
+++ src/components/PrivacyPolicy/PrivacyDialog.js
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
-
 import './PrivacyDialog.css';
 
 const PrivacyDialog = () => {
   return (
     <div className='privacy-dialog'>
-      <h1>Privacy Notice</h1>
       <p>
         I use Google Analytics, cookies, and other technologies to help me
-        understand how you use this website.
+        understand how you use this website.{' '}
+        <Link to='/privacy-policy'>Privacy Policy</Link>
       </p>
-      <Link to='/privacy-policy'>Privacy Policy</Link>
     </div>
   );
 };
