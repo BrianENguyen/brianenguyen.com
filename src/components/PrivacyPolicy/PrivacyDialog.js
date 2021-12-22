e46a80968991c7288beae0b688f827a78bbf3bc7
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-22 11:55:25 -0800

    PrivacyDialog: close button

--- src/components/PrivacyPolicy/PrivacyDialog.js
+++ src/components/PrivacyPolicy/PrivacyDialog.js
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom';
+import { CgClose } from 'react-icons/cg';
+import { Grid } from '@mui/material';
 import './PrivacyDialog.css';
 
 const PrivacyDialog = () => {
   return (
     <div className='privacy-dialog'>
-      <p>
-        I use Google Analytics, cookies, and other technologies to help me
-        understand how you use this website.{' '}
-        <Link to='/privacy-policy'>Privacy Policy</Link>
-      </p>
+      <Grid container>
+        <Grid item xs={11}>
+          <p>
+            I use Google Analytics, cookies, and other technologies to help me
+            understand how you use this website.{' '}
+            <Link to='/privacy-policy'>Privacy Policy</Link>
+          </p>
+        </Grid>
+        <Grid item xs={1}>
+          <CgClose className='privacy-dialog__close-btn' />
+        </Grid>
+      </Grid>
     </div>
   );
 };
