9465750f5cac14c001b93173c118963a6f398355
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-22 11:20:28 -0800

    PrivacyDialog: content

--- src/components/PrivacyPolicy/PrivacyDialog.js
+++ src/components/PrivacyPolicy/PrivacyDialog.js
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const PrivacyDialog = () => {
-  return <div>Dialog</div>;
+  return (
+    <div>
+      <h1>Privacy Notice</h1>
+      <p>
+        I use Google Analytics, cookies, and other technologies to help me
+        understand how you use this website.
+      </p>
+      <Link to='/privacy-policy'>Privacy Policy</Link>
+    </div>
+  );
 };
 
 export default PrivacyDialog;
