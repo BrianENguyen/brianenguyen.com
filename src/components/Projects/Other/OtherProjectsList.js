051fe53ae1d8ad08f638be46b012eca3f0e1245f
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-24 11:16:07 -0800

    OtherProject: fixed card margin

--- src/components/Projects/Other/OtherProjectsList.js
+++ src/components/Projects/Other/OtherProjectsList.js
@@ -8,7 +8,7 @@ const OtherProjectsList = () => {
       <h1 className='other-projects__title'>Some of my other work</h1>
       <Grid container>
         {OtherProjectData.map((otherProject, i) => (
-          <Grid item xs={12} md={6} lg={4}>
+          <Grid item xs={12} md={4}>
             <OtherProject
               key={i}
               title={otherProject.title}
