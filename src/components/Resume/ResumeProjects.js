11744158be39b27c3cc3d627ca1e57700218d4a4
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-26 12:49:47 -0800

    ResumeProjects: links

--- src/components/Resume/ResumeProjects.js
+++ src/components/Resume/ResumeProjects.js
@@ -5,15 +5,17 @@ const ResumeProjects = () => {
     <div>
       <h2 className='resume__header'>Projects</h2>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={4} lg={6}>
+        <Grid item xs={12} sm={8}>
           <h3 className='resume__leftcolumn--header'>
             Remade Personal Portfolio |{' '}
             <span>
-              <a href='#'>Test</a>
+              <a href='https://bit.ly/BEN-Portfolio-Remade'>
+                bit.ly/BEN-Portfolio-Remade
+              </a>
             </span>
           </h3>
         </Grid>
-        <Grid item xs={12} md={8} lg={6}>
+        <Grid item xs={12} sm={4}>
           <h3 className='resume__rightcolumn--header'>January 2022</h3>
         </Grid>
         <Grid item xs={12}>
@@ -28,12 +30,12 @@ const ResumeProjects = () => {
           <h3 className='resume__leftcolumn--header'>
             Thu Nguyen’s Website |{' '}
             <span>
-              <a href='#'>Test</a>
+              <a href='https://bit.ly/BEN-Thu'>bit.ly/BEN-Thu</a>
             </span>
           </h3>
         </Grid>
         <Grid item xs={12} md={8} lg={6}>
-          <h3 className='resume__rightcolumn--header'>November 2021</h3>
+          <h3 className='resume__rightcolumn--header'>December 2021</h3>
         </Grid>
         <Grid item xs={12}>
           <p className='resume__leftcolumn--project-description'>
@@ -47,12 +49,12 @@ const ResumeProjects = () => {
           <h3 className='resume__leftcolumn--header'>
             CSULB Tzu Ching |{' '}
             <span>
-              <a href='#'>Test</a>
+              <a href='https://bit.ly/BEN-LBTC'>bit.ly/BEN-LBTC</a>
             </span>
           </h3>
         </Grid>
         <Grid item xs={12} md={8} lg={6}>
-          <h3 className='resume__rightcolumn--header'>April 2021</h3>
+          <h3 className='resume__rightcolumn--header'>November 2021</h3>
         </Grid>
         <Grid item xs={12}>
           <p className='resume__leftcolumn--project-description'>
@@ -69,12 +71,12 @@ const ResumeProjects = () => {
           <h3 className='resume__leftcolumn--header'>
             Personal Portfolio |{' '}
             <span>
-              <a href='#'>Test</a>
+              <a href='https://bit.ly/BEN-Portfolio'>bit.ly/BEN-Portfolio</a>
             </span>
           </h3>
         </Grid>
         <Grid item xs={12} md={8} lg={6}>
-          <h3 className='resume__rightcolumn--header'>January 2022</h3>
+          <h3 className='resume__rightcolumn--header'>April 2021</h3>
         </Grid>
         <Grid item xs={12}>
           <p className='resume__leftcolumn--project-description'>
