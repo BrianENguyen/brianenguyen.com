690356e30afb01c0a663fdfc9289fc8c7fc25ee3
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-26 12:22:03 -0800

    ResumeProjects: initial layout

---
+++ src/components/Resume/ResumeProjects.js
@@ -0,0 +1,87 @@
+import { Grid } from '@mui/material';
+
+const ResumeProjects = () => {
+  return (
+    <div>
+      <h2 className='resume__header'>Projects</h2>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} lg={6}>
+          <h3 className='resume__leftcolumn--header'>
+            Remade Personal Portfolio |{' '}
+            <span>
+              <a href='#'>Test</a>
+            </span>
+          </h3>
+        </Grid>
+        <Grid item xs={12} md={8} lg={6}>
+          <h3 className='resume__rightcolumn--header'>January 2022</h3>
+        </Grid>
+        <Grid item xs={12}>
+          <p>
+            Redesigned and redeveloped version of my old website using React.js,
+            Material Design Framework, React Router, and EmailJS
+          </p>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} lg={6}>
+          <h3 className='resume__leftcolumn--header'>
+            Remade Personal Portfolio |{' '}
+            <span>
+              <a href='#'>Test</a>
+            </span>
+          </h3>
+        </Grid>
+        <Grid item xs={12} md={8} lg={6}>
+          <h3 className='resume__rightcolumn--header'>January 2022</h3>
+        </Grid>
+        <Grid item xs={12}>
+          <p>
+            Redesigned and redeveloped version of my old website using React.js,
+            Material Design Framework, React Router, and EmailJS
+          </p>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} lg={6}>
+          <h3 className='resume__leftcolumn--header'>
+            Remade Personal Portfolio |{' '}
+            <span>
+              <a href='#'>Test</a>
+            </span>
+          </h3>
+        </Grid>
+        <Grid item xs={12} md={8} lg={6}>
+          <h3 className='resume__rightcolumn--header'>January 2022</h3>
+        </Grid>
+        <Grid item xs={12}>
+          <p>
+            Redesigned and redeveloped version of my old website using React.js,
+            Material Design Framework, React Router, and EmailJS
+          </p>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} lg={6}>
+          <h3 className='resume__leftcolumn--header'>
+            Remade Personal Portfolio |{' '}
+            <span>
+              <a href='#'>Test</a>
+            </span>
+          </h3>
+        </Grid>
+        <Grid item xs={12} md={8} lg={6}>
+          <h3 className='resume__rightcolumn--header'>January 2022</h3>
+        </Grid>
+        <Grid item xs={12}>
+          <p>
+            Redesigned and redeveloped version of my old website using React.js,
+            Material Design Framework, React Router, and EmailJS
+          </p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ResumeProjects;
