0aa6f7911e3c29c734a5b0efb66a60c796727642
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-27 11:50:53 -0800

    Resume: download links

--- src/views/ResumeView.js
+++ src/views/ResumeView.js
@@ -1,15 +1,33 @@
 import Jumbotron from '../components/UI/Jumbotron';
 import Resume from '../components/Resume/Resume';
+import BtnStandard from '../components/UI/Button/BtnStandard';
 
 const ResumeView = () => {
   const jumboText = 'Resume';
   const jumboImage =
     'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240498/BEN_Website/BEN_lagunabeach_k9dv2n.jpg';
   return (
-    // TODO: resume
-    <div className='portfolio'>
+    <div>
       <Jumbotron image={jumboImage} title={jumboText} />
+      <center>
+        <a
+          href='https://u.pcloud.link/publink/show?code=XZ4BKaXZjSqBAzUDjlBJhTfrfj0uvYc9bp70'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <BtnStandard>Download Resume</BtnStandard>
+        </a>
+      </center>
       <Resume />
+      <center>
+        <a
+          href='https://u.pcloud.link/publink/show?code=XZ4BKaXZjSqBAzUDjlBJhTfrfj0uvYc9bp70'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <BtnStandard>Download Resume</BtnStandard>
+        </a>
+      </center>
     </div>
   );
 };
