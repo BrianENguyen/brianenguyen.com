2f8d77d0beac9c03e38ccddc87501522ff50fdb9
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-27 12:12:09 -0800

    Resume: bottom margin

--- src/views/ResumeView.js
+++ src/views/ResumeView.js
@@ -7,7 +7,7 @@ const ResumeView = () => {
   const jumboImage =
     'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240498/BEN_Website/BEN_lagunabeach_k9dv2n.jpg';
   return (
-    <div>
+    <div style={{ marginBottom: 3 + 'rem' }}>
       <Jumbotron image={jumboImage} title={jumboText} />
       <center>
         <a
