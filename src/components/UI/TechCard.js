71aa1c22d980f53575ffaf42c50e5e6e724862c1
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-28 13:25:25 -0800

    TechCard: animations

--- src/components/UI/TechCard.js
+++ src/components/UI/TechCard.js
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import Card from '@mui/material/Card';
+import Aos from 'aos';
 
 import './TechCard.css';
 const TechCard = (props) => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
-    <Card className='tech-card'>
+    <Card className='tech-card' data-aos='zoom-out-up'>
       <div className='tech-card__image'>{props.image} </div>
       <div className='tech-card__title'>{props.title}</div>
     </Card>
