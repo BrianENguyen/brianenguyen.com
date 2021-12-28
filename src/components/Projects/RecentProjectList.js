465bf69af144cf0928a67b33e0eddd6354a327d7
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-28 13:20:56 -0800

    RecentProjects: animations based on key

--- src/components/Projects/RecentProjectList.js
+++ src/components/Projects/RecentProjectList.js
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
+
 import { Grid } from '@mui/material';
+import Aos from 'aos';
 
 import Project from './Project';
 import ProjectData from '../Helper/ProjectData';
 import './RecentProjects.css';
 const RecentProjects = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className='recent-projects'>
       <h1 className='recent-projects__header'>Recent Projects</h1>
@@ -12,7 +19,13 @@ const RecentProjects = () => {
       </h1>
       <Grid container spacing={2}>
         {ProjectData.slice(0, 2).map((project, i) => (
-          <Grid item xs={12} sm={6} key={i}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            key={i}
+            data-aos={i % 2 == 0 ? 'fade-right' : 'fade-left'}
+          >
             <Project
               title={project.title}
               src={project.src}
