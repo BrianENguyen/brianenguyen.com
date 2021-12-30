8bd4985fe96ad982ce0cf0b824768daa85bf803d
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-29 23:50:21 -0800

    h2 to h1

--- src/components/About/Backstory.js
+++ src/components/About/Backstory.js
@@ -7,7 +7,7 @@ const Backstory = () => {
       <Container maxWidth='xl'>
         <Grid container spacing={2}>
           <Grid item xs={12} md={4}>
-            <h2 className='backstory__header'>My Story</h2>
+            <h1 className='backstory__header'>My Story</h1>
           </Grid>
           <Grid item xs={12} md={8}>
             <p>
