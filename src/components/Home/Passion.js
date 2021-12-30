530b2ec377c083ce17941102bcac69d1d5576e02
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-29 17:53:00 -0800

    Passion: fixed some words

--- src/components/Home/Passion.js
+++ src/components/Home/Passion.js
@@ -19,14 +19,14 @@ const Passion = () => {
             <div className='passion__text'>
               <h2>My passion is front-end web development</h2>
               <p>
-                I love making websites look beautiful. I have years of
+                I love making beautiful websites. I have at least 3 years of
                 experience being a web developer with many different
                 technologies, such as HTML/CSS/JS, Vue, React, and more. If you
                 need a website made, then feel free to reach out to me
               </p>
               <center>
                 <Link to='/portfolio'>
-                  <BtnStandard>View Work</BtnStandard>
+                  <BtnStandard>View My Work</BtnStandard>
                 </Link>
               </center>
             </div>
