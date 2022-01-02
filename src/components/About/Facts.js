e6578c3683da1a944f522cf386c74c9f1d1a1718
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-02 13:59:51 -0800

    Facts: content

--- src/components/About/Facts.js
+++ src/components/About/Facts.js
@@ -1,14 +1,25 @@
 import { Container } from '@mui/material';
-
+import { Grid } from '@mui/material';
 const Facts = () => {
   return (
     <div>
-      <Container>
-        <h1 className='header'>Things that define me</h1>
-        <h2 className='subheader'>
-          I am a front-end developer, drummer, gamer, pro-wrestling enjoyer, and
-          Linkin Park listener
-        </h2>
+      <Container maxWidth='xl'>
+        <Grid container spacing={30}>
+          <Grid item xs={12} sm={12} lg={4}>
+            <img
+              src='https://res.cloudinary.com/buraiyen/image/upload/v1641160435/BEN_Website/photo-gallery/BEN_mimi.jpg'
+              width='450px'
+              alt='Me holding Mimi'
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} lg={8}>
+            <h1 className='header'>Things that define me</h1>
+            <h2 className='subheader'>
+              I am a front-end developer, photographer, drummer, gamer,
+              pro-wrestling enjoyer, and Linkin Park listener
+            </h2>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
