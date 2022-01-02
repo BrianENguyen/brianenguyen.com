d3be3942af6c9ba9feb080696f5ba649e45637a7
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-02 13:42:42 -0800

    Facts: text

---
+++ src/components/About/Facts.js
@@ -0,0 +1,17 @@
+import { Container } from '@mui/material';
+
+const Facts = () => {
+  return (
+    <div>
+      <Container>
+        <h1 className='header'>Things that define me</h1>
+        <h2 className='subheader'>
+          I am a front-end developer, drummer, gamer, pro-wrestling enjoyer, and
+          Linkin Park listener
+        </h2>
+      </Container>
+    </div>
+  );
+};
+
+export default Facts;
