1c47f90e38aa790511bb9d35ff484ef6bb3761c1
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-03 12:33:01 -0800

    Facts: improved responsiveness

--- src/components/About/Facts.js
+++ src/components/About/Facts.js
@@ -4,7 +4,7 @@ const Facts = () => {
   return (
     <div className='facts'>
       <Container>
-        <Grid container spacing={2}>
+        <Grid container spacing={5}>
           <Grid item sm={12} md={4}>
             <img
               src='https://res.cloudinary.com/buraiyen/image/upload/v1641160435/BEN_Website/photo-gallery/BEN_mimi.jpg'
@@ -13,11 +13,13 @@ const Facts = () => {
             />
           </Grid>
           <Grid item sm={12} md={8}>
-            <h1 className='header'>Things that define me</h1>
-            <h2 className='subheader'>
-              I am a front-end developer, photographer, drummer, gamer,
-              pro-wrestling enjoyer, and Linkin Park listener
-            </h2>
+            <div className='facts__section'>
+              <h1 className='header'>Things that define me</h1>
+              <h2 className='subheader'>
+                I am a front-end developer, photographer, drummer, gamer,
+                pro-wrestling enjoyer, and Linkin Park listener
+              </h2>
+            </div>
           </Grid>
         </Grid>
       </Container>
