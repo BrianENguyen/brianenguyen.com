812543847ee151d530978d3ee08ad2006c69d37a
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-03 12:16:50 -0800

    Facts: responsive image

--- src/components/About/Facts.js
+++ src/components/About/Facts.js
@@ -4,14 +4,14 @@ const Facts = () => {
   return (
     <div className='quote'>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
+        <Grid item sm={12} md={4}>
           <img
             src='https://res.cloudinary.com/buraiyen/image/upload/v1641160435/BEN_Website/photo-gallery/BEN_mimi.jpg'
             alt='Me holding Mimi'
             className='facts__image'
           />
         </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid item sm={12} md={8}>
           <h1 className='header'>Things that define me</h1>
           <h2 className='subheader'>
             I am a front-end developer, photographer, drummer, gamer,
