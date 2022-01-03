6aa34c147ee02b7bf7bd5ca6e0a94f4164e6b2fa
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-03 12:19:27 -0800

    Facts: container

--- src/components/About/Facts.js
+++ src/components/About/Facts.js
@@ -1,24 +1,26 @@
-import { Grid } from '@mui/material';
+import { Grid, Container } from '@mui/material';
 import './Facts.css';
 const Facts = () => {
   return (
-    <div className='quote'>
-      <Grid container spacing={2}>
-        <Grid item sm={12} md={4}>
-          <img
-            src='https://res.cloudinary.com/buraiyen/image/upload/v1641160435/BEN_Website/photo-gallery/BEN_mimi.jpg'
-            alt='Me holding Mimi'
-            className='facts__image'
-          />
+    <div className='facts'>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item sm={12} md={4}>
+            <img
+              src='https://res.cloudinary.com/buraiyen/image/upload/v1641160435/BEN_Website/photo-gallery/BEN_mimi.jpg'
+              alt='Me holding Mimi'
+              className='facts__image'
+            />
+          </Grid>
+          <Grid item sm={12} md={8}>
+            <h1 className='header'>Things that define me</h1>
+            <h2 className='subheader'>
+              I am a front-end developer, photographer, drummer, gamer,
+              pro-wrestling enjoyer, and Linkin Park listener
+            </h2>
+          </Grid>
         </Grid>
-        <Grid item sm={12} md={8}>
-          <h1 className='header'>Things that define me</h1>
-          <h2 className='subheader'>
-            I am a front-end developer, photographer, drummer, gamer,
-            pro-wrestling enjoyer, and Linkin Park listener
-          </h2>
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 };
