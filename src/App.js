b13eda2501f21069e407f00f6dab4156b972b653
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-21 17:34:56 -0800

    import RenderRoutes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { Route, Switch } from 'react-router-dom';
-
-import HomeView from './views/HomeView';
-import AboutView from './views/AboutView';
-import PortfolioView from './views/PortfolioView';
-import ResumeView from './views/ResumeView';
-import ContactView from './views/ContactView';
-import ErrorView from './views/ErrorView';
-import PrivacyView from './views/PrivacyView';
-import FrontendMentorView from './views/project/FrontendMentorView';
-import NewSiteView from './views/project/NewSiteView';
-import ThuView from './views/project/ThuView';
-import LBTCView from './views/project/LBTCView';
-import YelpCamp from './views/project/YelpCamp';
-import FirstSiteView from './views/project/FirstSiteView';
-import KrakenBracketView from './views/project/KrakenBracketView';
-import SIMDView from './views/project/SIMDView';
-import ProbAndStatsView from './views/project/ProbAndStatsView';
 import Navbar from './components/UI/Navbar/Navbar';
 import Footer from './components/UI/Footer/Footer';
 import PrivacyDialog from './components/Privacy/Dialog/PrivacyDialog';
+import RenderRoutes from './components/Router/RenderRoutes';
 import './App.css';
 
 import ScrollToTop from './components/Helper/ScrollToTop';
@@ -29,27 +12,7 @@ function App() {
       <Navbar />
       <header className='App-header'>
         <ScrollToTop />
-        <Switch>
-          <Route exact path='/' component={HomeView} />
-          <Route path='/about' component={AboutView} />
-          <Route path='/portfolio' component={PortfolioView} />
-          <Route path='/resume' component={ResumeView} />
-          <Route path='/contact' component={ContactView} />
-          <Route
-            path='/frontend-mentor-challenges'
-            component={FrontendMentorView}
-          />
-          <Route path='/new-site' component={NewSiteView} />
-          <Route path='/thu-nguyen' component={ThuView} />
-          <Route path='/csulb-tzu-ching' component={LBTCView} />
-          <Route path='/yelpcamp' component={YelpCamp} />
-          <Route path='/first-personal-site' component={FirstSiteView} />
-          <Route path='/kraken-bracket' component={KrakenBracketView} />
-          <Route path='/simd' component={SIMDView} />
-          <Route path='/probability-stats' component={ProbAndStatsView} />
-          <Route path='/privacy-policy' component={PrivacyView} />
-          <Route path='*' component={ErrorView} />
-        </Switch>
+        <RenderRoutes />
       </header>
       <PrivacyDialog />
       <Footer />
