9f61089c3484a3fd7909e24e60d4dfbfeb1011f8
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-21 17:21:56 -0800

    RenderRoutes function

---
+++ src/components/Router/RenderRoutes.js
@@ -0,0 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
+import ROUTES from './Routes';
+const RenderRoutes = () => {
+  return (
+    <Switch>
+      {ROUTES.map((route) => (
+        <Route
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+        />
+      ))}
+    </Switch>
+  );
+};
+
+export default RenderRoutes;
