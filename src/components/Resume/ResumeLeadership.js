b5c6d62c19b1178d14fa1ad784d7fed1b527fd1f
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-26 11:21:51 -0800

    ResumeLeadership content and import

--- src/components/Resume/ResumeLeadership.js
+++ src/components/Resume/ResumeLeadership.js
@@ -5,35 +5,37 @@ import './Resume.css';
 const ResumeLeadership = () => {
   return (
     <div>
-      <h2 className='resume__header'>Work Experience</h2>
+      <h2 className='resume__header'>Leadership</h2>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4} lg={6}>
-          <h3 className='resume__leftcolumn--header'>Code Ninjas</h3>
+          <h3 className='resume__leftcolumn--header'>CSULB Tzu Ching</h3>
         </Grid>
         <Grid item xs={12} md={8} lg={6}>
-          <h3 className='resume__rightcolumn--header'>Aug. 2020 – Present</h3>
+          <h3 className='resume__rightcolumn--header'>Aug. 2019 – May 2020</h3>
         </Grid>
         <Grid item xs={12} md={4} lg={6}>
-          <p className='resume__leftcolumn--description'>Coding Instructor</p>
+          <p className='resume__leftcolumn--description'>President</p>
         </Grid>
         <Grid item xs={12} md={8} lg={6}>
-          <p className='resume__rightcolumn--description'>
-            Cerritos and Fullerton, CA
-          </p>
+          <p className='resume__rightcolumn--description'>Long Beach, CA</p>
         </Grid>
         <div>
           <ul className='resume__list'>
             <li>
-              Tutored children on programming fundamentals using JavaScript,
-              Lua, C#, and Scratch coding languages
+              Managed organization with 3 board members to host monthly general
+              meetings and volunteering events for general members to attend
             </li>
             <li>
-              Assisted children in creating video games and helped debugged
-              errors, which increased the completion rate by 50%
+              Created warm and welcoming experiences to event attendees which
+              incentivized them to return to future events
             </li>
             <li>
-              Developed and led multiple STEM and game building camps that had
-              an overall 95% satisfaction rate
+              Managed organization’s files on Google Drive; taught office tool
+              fundamentals to board members
+            </li>
+            <li>
+              Helped revitalize student interest in the organization; gained a
+              200% increase in new members during the 2019-2020 school year
             </li>
           </ul>
         </div>
