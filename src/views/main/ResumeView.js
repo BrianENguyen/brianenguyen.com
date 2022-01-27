8efdc6bd72a1f6c8d1c58445c1ddd23a9c52afe2
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:06:46 -0800

    import Resume Jumbotron data

--- src/views/main/ResumeView.js
+++ src/views/main/ResumeView.js
@@ -2,13 +2,13 @@ import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
 import Resume from '../../components/Resume/Resume';
 import BtnStandard from '../../components/UI/Button/BtnStandard';
 import { TitleChange } from '../../components/Helper/TitleChange';
+import { ResumeData } from '../../components/Helper/JumbotronData';
 
 const ResumeView = () => {
   TitleChange('Resume | BEN');
 
-  const jumboText = 'Resume';
-  const jumboImage =
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1447,b_black,o_70/v1620240498/BEN_Website/BEN_lagunabeach_k9dv2n.jpg';
+  const jumboText = ResumeData.title;
+  const jumboImage = ResumeData.image;
 
   return (
     <div style={{ marginBottom: 3 + 'rem' }}>
