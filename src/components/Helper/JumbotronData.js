aafaf3bc6a5b16a07fef8ffed5739e70a4243d5a
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:22:01 -0800

    JumboData: about data nad scaled images

--- src/components/Helper/JumbotronData.js
+++ src/components/Helper/JumbotronData.js
@@ -1,6 +1,6 @@
 const HomeData = {
   image:
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
   title: 'Brian E. Nguyen',
   description:
     '2020 Computer Science Graduate and Aspiring Front-end Developer',
@@ -8,14 +8,20 @@ const HomeData = {
 
 const ResumeData = {
   image:
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_laptop.jpg',
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_laptop.jpg',
   title: 'Resume',
 };
 
 const PortfolioData = {
   image:
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_code_hijtco.jpg',
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_code_hijtco.jpg',
   title: 'Portfolio',
 };
 
-export { HomeData, ResumeData, PortfolioData };
+const AboutData = {
+  image:
+    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_legoart.jpg',
+  title: 'About',
+};
+
+export { HomeData, ResumeData, PortfolioData, AboutData };
