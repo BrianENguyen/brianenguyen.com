d56b2870541acfcf4dae66c285fd98be62cb64d6
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:02:56 -0800

    changed home view image

--- src/components/Helper/JumbotronData.js
+++ src/components/Helper/JumbotronData.js
@@ -1,6 +1,6 @@
 const HomeData = {
   image:
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg',
+    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
   title: 'Brian E. Nguyen',
   description:
     '2020 Computer Science Graduate and Aspiring Front-end Developer',
