d2327b7852fb96ce3450fa622d16466742bd6802
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:04:08 -0800

    HomeView jumbo data imports

--- src/views/main/HomeView.js
+++ src/views/main/HomeView.js
@@ -5,16 +5,14 @@ import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
 import Passion from '../../components/Home/Passion';
 import RecentProjectList from '../../components/Projects/Main/RecentProjectList';
 import WebCardList from '../../components/Home/WebCardList';
+import { HomeData } from '../../components/Helper/JumbotronData';
 
 const HomeView = () => {
   TitleChange('Brian E. Nguyen');
 
-  // TODO: change jumbo image to me coding
-  const jumboImage =
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v1620240501/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg';
-  const jumboTitle = 'Brian E. Nguyen';
-  const jumboDescription =
-    '2020 Computer Science Graduate and Aspiring Front-end Developer';
+  const jumboImage = HomeData.image;
+  const jumboTitle = HomeData.title;
+  const jumboDescription = HomeData.description;
 
   return (
     <div className='home'>
