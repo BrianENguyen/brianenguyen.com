d8e4095cc5ea8f9b165bbc8fd3e8bb2001039996
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:00:33 -0800

    reworked logic of JumbotronData

--- src/components/Helper/JumbotronData.js
+++ src/components/Helper/JumbotronData.js
@@ -1,11 +1,9 @@
-const JumbotronData = [
-  {
-    home: {
-      image:
-        'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg',
-      title: 'Brian E. Nguyen',
-      description:
-        '2020 Computer Science Graduate and Aspiring Front-end Developer',
-    },
-  },
-];
+const HomeData = {
+  image:
+    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg',
+  title: 'Brian E. Nguyen',
+  description:
+    '2020 Computer Science Graduate and Aspiring Front-end Developer',
+};
+
+export { HomeData };
