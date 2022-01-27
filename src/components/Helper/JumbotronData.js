63429ff4374c3cce84177e2e0bf1b387be50225c
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-27 12:05:36 -0800

    JumbotronData: resume

--- src/components/Helper/JumbotronData.js
+++ src/components/Helper/JumbotronData.js
@@ -6,4 +6,10 @@ const HomeData = {
     '2020 Computer Science Graduate and Aspiring Front-end Developer',
 };
 
-export { HomeData };
+const ResumeData = {
+  image:
+    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_laptop.jpg',
+  title: 'Resume',
+};
+
+export { HomeData, ResumeData };
