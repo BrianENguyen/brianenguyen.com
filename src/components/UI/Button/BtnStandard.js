6f102bf0a10acb5e2ca04458679c717ec3b268e6
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-06 13:40:56 -0800

    deleted Btn files

--- src/components/UI/Button/BtnStandard.js
+++
@@ -1,7 +0,0 @@
-import './Buttons.css';
-
-const BtnStandard = (props) => {
-  return <button className='btn btn-standard'>{props.children}</button>;
-};
-
-export default BtnStandard;
--- src/components/UI/Button/BtnStandardLight.js
+++
@@ -1,7 +0,0 @@
-import './Buttons.css';
-
-const BtnStandardLight = (props) => {
-  return <button className='btn btn-standard-light'>{props.children}</button>;
-};
-
-export default BtnStandardLight;
