b338615366e3bb41263c2d72ca3ca36827c9c371
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-06 13:21:28 -0800

    Button starting layout

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,13 @@
+import './Buttons.css';
 const Button = (props) => {
-  return <button>{props.children}</button>;
+  return (
+    <button
+      className={`btn ${props.className} `}
+      disabled={props.disabled === 0 && true}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
