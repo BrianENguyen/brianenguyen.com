2d2b9e61ab0c222cd7b471d77dd76e7847c3a3ba
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-09 12:17:11 -0800

    init TitleData

---
+++ src/components/Helper/TitleData.js
@@ -0,0 +1,47 @@
+const HomeTitle = '';
+
+const ResumeTitle = '';
+
+const PortfolioTitle = '';
+
+const AboutTitle = '';
+
+const ContactTitle = '';
+
+const ErrorTitle = '';
+
+const FirstSiteTitle = '';
+
+const FrontendMentorTitle = '';
+
+const KrakenBracketTitle = '';
+
+const LBTCTitle = '';
+
+const NewSiteTitle = '';
+
+const ProbAndStatsTitle = '';
+
+const SIMDTitle = '';
+
+const ThuTitle = '';
+
+const YelpCampTitle = '';
+
+export {
+  HomeTitle,
+  ResumeTitle,
+  PortfolioTitle,
+  AboutTitle,
+  ContactTitle,
+  ErrorTitle,
+  FirstSiteTitle,
+  FrontendMentorTitle,
+  KrakenBracketTitle,
+  LBTCTitle,
+  NewSiteTitle,
+  ProbAndStatsTitle,
+  SIMDTitle,
+  ThuTitle,
+  YelpCampTitle,
+};
