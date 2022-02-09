67685b7400b9336d7127751a399830146674dc99
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-09 12:26:32 -0800

    renamed PageTitleData file; populated data

---
+++ src/components/Helper/PageTitleData.js
@@ -0,0 +1,49 @@
+const suffix = '| BEN';
+
+const HomePageTitle = 'Brian E. Nguyen | Frontend Developer';
+
+const ResumePageTitle = `Resume ${suffix}`;
+
+const PortfolioPageTitle = `Portfolio ${suffix}`;
+
+const AboutPageTitle = `About ${suffix}`;
+
+const ContactPageTitle = `Contact ${suffix}`;
+
+const ErrorPageTitle = `404 ${suffix}`;
+
+const FirstSitePageTitle = `My First Website ${suffix}`;
+
+const FrontendMentorPageTitle = `Frontend Mentor Challenges ${suffix}`;
+
+const KrakenBracketPageTitle = `Kraken Bracket ${suffix}`;
+
+const LBTCPageTitle = `CSULB Tzu Ching ${suffix}`;
+
+const NewSitePageTitle = `My New Website ${suffix}`;
+
+const ProbAndStatsPageTitle = `Probability and Stats ${suffix}`;
+
+const SIMDPageTitle = `SIMD ${suffix}`;
+
+const ThuPageTitle = `Thu Nguyen ${suffix}`;
+
+const YelpCampPageTitle = `YelpCamp ${suffix}`;
+
+export {
+  HomePageTitle,
+  ResumePageTitle,
+  PortfolioPageTitle,
+  AboutPageTitle,
+  ContactPageTitle,
+  ErrorPageTitle,
+  FirstSitePageTitle,
+  FrontendMentorPageTitle,
+  KrakenBracketPageTitle,
+  LBTCPageTitle,
+  NewSitePageTitle,
+  ProbAndStatsPageTitle,
+  SIMDPageTitle,
+  ThuPageTitle,
+  YelpCampPageTitle,
+};
