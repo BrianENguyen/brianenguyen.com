9532c6d9dc9594878947e80bd60e80f007e3d405
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-09 13:17:06 -0800

    init AnimatedPage.js

---
+++ src/components/Animations/AnimatedPage.js
@@ -0,0 +1,22 @@
+import { motion } from 'framer-motion/dist/framer-motion';
+
+const animations = {
+  initial: { opacity: 0, x: 0 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 0 },
+};
+
+const AnimatedPage = ({ children }) => {
+  return (
+    <motion.div
+      variants={animations}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimatedPage;
