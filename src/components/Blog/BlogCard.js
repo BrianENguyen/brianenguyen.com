dfef83a761d9d926b0a251fe16c08c38a5f1a253
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 13:18:23 -0800

    BlogCard: image field

--- src/components/Blog/BlogCard.js
+++ src/components/Blog/BlogCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const BlogCard = (props) => {
   return (
     <div className={classes['blog-card']}>
+      <img src={props.image} alt='' />
       <h1 className={classes['blog-card__title']}>{props.title}</h1>
       <p className={classes['blog-card__description']}>{props.description}</p>
       <Link to='#' className={classes['blog-card__link']}>
