5898962565a3b97248353d4a8e275f992496ce70
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 13:31:18 -0800

    BlogList: fixed responsiveness

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -2,13 +2,14 @@ import BlogCard from './BlogCard';
 import BlogData from '../Helper/BlogData';
 import { Grid } from '@mui/material';
 import { Container } from '@mui/material';
+
 const BlogList = () => {
   return (
     <div className='blog-list'>
       <Container>
         <Grid container>
           {BlogData.map((blog, i) => (
-            <Grid item xs={12} sm={4} key={i}>
+            <Grid item xs={12} sm={6} md={4} key={i}>
               <BlogCard
                 title={blog.title}
                 description={blog.description}
