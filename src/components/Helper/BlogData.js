908f3de3039bfb018928710ca7d1146ef67e794e
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 13:07:12 -0800

    first data for BlogData

--- src/components/Helper/BlogData.js
+++ src/components/Helper/BlogData.js
@@ -1,2 +1,10 @@
-const BlogData = {};
+const BlogData = [
+  {
+    title: "Why I'm Blogging",
+    description: "Recently, I decided that I will start blogging. Here's why.",
+    image: '',
+    link: '',
+  },
+];
+
 export default BlogData;
