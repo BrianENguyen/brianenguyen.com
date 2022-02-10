f40d26daff65c6726256472fd3667d8935c7d945
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 13:33:22 -0800

    BlogView: animated page

--- src/views/main/BlogView.js
+++ src/views/main/BlogView.js
@@ -1,20 +1,24 @@
 import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
 import { BlogData } from '../../components/Helper/JumbotronData';
 import BlogList from '../../components/Blog/BlogList';
+import AnimatedPage from '../../components/Animations/AnimatedPage';
+
 const BlogView = () => {
   const jumboTitle = BlogData.title;
   const jumboImage = BlogData.image;
   const jumboDescription = BlogData.description;
 
   return (
-    <div className='blog'>
-      <Jumbotron
-        image={jumboImage}
-        title={jumboTitle}
-        description={jumboDescription}
-      />
-      <BlogList />
-    </div>
+    <AnimatedPage>
+      <div className='blog'>
+        <Jumbotron
+          image={jumboImage}
+          title={jumboTitle}
+          description={jumboDescription}
+        />
+        <BlogList />
+      </div>
+    </AnimatedPage>
   );
 };
 export default BlogView;
