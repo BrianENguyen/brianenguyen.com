4c1638806186c504532dda334c9036bf1b6aefa5
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 12:52:26 -0800

    init BlogView jumbotron

--- src/views/main/BlogView.js
+++ src/views/main/BlogView.js
@@ -1,4 +1,19 @@
+import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
+import { BlogData } from '../../components/Helper/JumbotronData';
+
 const BlogView = () => {
-  return <div>BlogView!</div>;
+  const jumboTitle = BlogData.title;
+  const jumboImage = BlogData.image;
+  const jumboDescription = BlogData.description;
+
+  return (
+    <div className='blog'>
+      <Jumbotron
+        image={jumboImage}
+        title={jumboTitle}
+        description={jumboDescription}
+      ></Jumbotron>
+    </div>
+  );
 };
 export default BlogView;
