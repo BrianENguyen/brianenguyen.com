ee89d6f762b3fd1e8e31f9e43f0ba8ab65eaadc2
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-10 13:28:38 -0800

    BlogList: added container

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,17 +1,24 @@
 import BlogCard from './BlogCard';
 import BlogData from '../Helper/BlogData';
+import { Grid } from '@mui/material';
+import { Container } from '@mui/material';
 const BlogList = () => {
   return (
     <div className='blog-list'>
-      {BlogData.map((blog, i) => (
-        <BlogCard
-          key={i}
-          title={blog.title}
-          description={blog.description}
-          image={blog.image}
-          link={blog.link}
-        />
-      ))}
+      <Container>
+        <Grid container>
+          {BlogData.map((blog, i) => (
+            <Grid item xs={12} sm={4} key={i}>
+              <BlogCard
+                title={blog.title}
+                description={blog.description}
+                image={blog.image}
+                link={blog.link}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 };
