1f809cddef3cf03eae004df96b740d2807b4f416
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-11 14:37:44 -0800

    changed blog class names to adhere to BEM

--- src/views/blog/WhyBlogView.js
+++ src/views/blog/WhyBlogView.js
@@ -12,16 +12,16 @@ const WhyBlogView = () => {
   return (
     <AnimatedPage>
       <Container style={{ marginTop: 200 + 'px' }} maxWidth='md'>
-        <h1 className={classes['blog-title']}>Why I'm Blogging</h1>
-        <div className={classes['blog-text']}>
-          <h2 className={classes['blog-header']}>The Purpose</h2>
+        <h1 className={classes['blog__title']}>Why I'm Blogging</h1>
+        <div className={classes['blog__text']}>
+          <h2 className={classes['blog__header']}>The Purpose</h2>
           <p>
             Something that I've noticed recently is that many tutorials that I
             find online come from blogs. I wanted to share my knowledge and my
             life, as well as improve my writing skills, so that's why I decided
             to create one as well.
           </p>
-          <h2 className={classes['blog-header']}>What can you expect?</h2>
+          <h2 className={classes['blog__header']}>What can you expect?</h2>
           <p>
             As of writing, I plan on providing my blogs with these kinds of
             content:
