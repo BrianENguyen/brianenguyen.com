1f413aad5d166b47acb104f928117a8dd51cb7df
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-12 11:25:04 -0800

    init WhyFrontendView

---
+++ src/views/blog/WhyFrontendView.js
@@ -0,0 +1,9 @@
+import classes from './BlogView.module.css';
+import AnimatedPage from '../../components/Animations/AnimatedPage';
+import { Container } from '@mui/material';
+import { useEffect } from 'react';
+import { WhyBlogPageTitle } from '../../components/Helper/PageTitleData';
+
+const WhyFrontendView = () => {};
+
+export default WhyFrontendView;
