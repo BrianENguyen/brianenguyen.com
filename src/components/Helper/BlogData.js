f3bb1dd6b54cc283b3f5162ba26d58446d87e8a1
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-11 19:13:05 -0800

    BlogData: WhyFrontend data

--- src/components/Helper/BlogData.js
+++ src/components/Helper/BlogData.js
@@ -1,5 +1,14 @@
 const BlogData = [
   {
+    title: 'Why I Chose Frontend Development',
+    description:
+      'I was conflicted in what I wanted to do with my CS degree after graduation, but it took me 2 years to choose frontend development.',
+    image:
+      'https://res.cloudinary.com/buraiyen/image/upload/v1644628220/BEN_Website/blog/BEN_4121_1_jw8m5b.jpg',
+    date: '2022-02-11',
+    link: '/why-frontend',
+  },
+  {
     title: "Why I'm Blogging",
     description: "Recently, I decided that I will start blogging. Here's why.",
     image:
