56e5c2d389a726e18e4a37a73163770272ee240e
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-12 11:31:50 -0800

    WhyFrontend: init content

--- src/views/blog/WhyFrontendView.js
+++ src/views/blog/WhyFrontendView.js
@@ -2,8 +2,25 @@ import classes from './BlogView.module.css';
 import AnimatedPage from '../../components/Animations/AnimatedPage';
 import { Container } from '@mui/material';
 import { useEffect } from 'react';
-import { WhyBlogPageTitle } from '../../components/Helper/PageTitleData';
+import { WhyFrontendPageTitle } from '../../components/Helper/PageTitleData';
 
-const WhyFrontendView = () => {};
+const WhyFrontendView = () => {
+  useEffect(() => {
+    document.title = WhyFrontendPageTitle;
+  }, []);
+
+  return (
+    <AnimatedPage>
+      <Container style={{ marginTop: 200 + 'px' }} maxWidth='md'>
+        <h1 className={classes['blog__title']}>
+          Why I Chose Frontend Development
+        </h1>
+        <div className={classes['blog__text']}>
+          <p className={classes['blog__date']}>?????????????????</p>
+        </div>
+      </Container>
+    </AnimatedPage>
+  );
+};
 
 export default WhyFrontendView;
