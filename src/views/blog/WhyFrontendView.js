7edf5be139a4e1fa4ef81607db6fa00dad04e985
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-12 11:43:25 -0800

    WhyFrontend: first paragraph

--- src/views/blog/WhyFrontendView.js
+++ src/views/blog/WhyFrontendView.js
@@ -17,6 +17,14 @@ const WhyFrontendView = () => {
         </h1>
         <div className={classes['blog__text']}>
           <p className={classes['blog__date']}>?????????????????</p>
+          <p>
+            Computer science is definitely one of the most interesting fields in
+            this world. With the exponential growth of technology, code is
+            basically everywhere, like gaming consoles, TV's, even the web
+            browser you're using to view this blog. As someone who is fascinated
+            with technology since my childhood, choosing a career path related
+            to technology was the right choice for me
+          </p>
         </div>
       </Container>
     </AnimatedPage>
