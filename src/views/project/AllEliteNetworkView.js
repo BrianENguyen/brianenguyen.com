8c1db22fb3d98404af37152d8cb925a7b9c19b22
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-05-03 13:20:38 -0700

    AllEliteNetworkView.js: intro

--- src/views/project/AllEliteNetworkView.js
+++ src/views/project/AllEliteNetworkView.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import { useEffect } from 'react';
+import AnimatedPage from '../../components/Animations/AnimatedPage';
+import { AllEliteNetworkData } from '../../components/Helper/JumbotronData';
+import { AllEliteNetworkPageTitle } from '../../components/Helper/PageTitleData';
+import Button from '../../components/UI/Button/Button';
+import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
+import './ProjectView.css';
 
 const AllEliteNetworkView = () => {
-  return <div>AllEliteNetworkView</div>;
+  const jumboTitle = AllEliteNetworkData.title;
+  useEffect(() => {
+    document.title = AllEliteNetworkPageTitle;
+  }, []);
+  return (
+    <AnimatedPage>
+      <div className='old-site'>
+        <Jumbotron title={jumboTitle} />
+        <Container maxWidth='md'>
+          <p className='project-view__description'>
+            <p>
+              All Elite Wrestling (AEW) is an American wrestling company based
+              in Jacksonville, Florida. Since its inception in 2019, AEW has
+              changed the landscape of professional wrestling. It provided a
+              great alternative for new and old wrestling fans, and is
+              considered the second largest professional wrestling company
+              behind Word Wrestling Entertainment (WWE).
+            </p>
+            <p>
+              For years, fans have been wanting a streaming service that
+              provides convenient access to live shows and archived content;
+              this includes AEW's quarterly Pay-Per Views, weekly TV shows
+              (Dynamite and Rampage), and weekly YouTube shows (Dark and Dark
+              Elevation)
+            </p>
+            <span className='project-view__description__title'>
+              The All Elite Network
+            </span>{' '}
+            is the ultimate unofficial streaming service for fans of All Elite
+            Wrestling to access the AEW library of amazing content
+          </p>
+          <div className='project-view__tech-stack'>
+            <h2>Tech Stack</h2>
+            <ul>
+              <li>React.js</li>
+              <li>SCSS</li>
+              <li>React Router</li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              <a
+                href='https://github.com/Brian-E-Nguyen/BEN-Website'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <Button className='btn-standard'>View Repository</Button>
+              </a>
+            </p>
+          </div>
+          <img src='' className='project-view__image' alt='' />
+        </Container>
+
+        <Container>
+          <div className='project-view__personal-notes'>
+            <h2>Personal Notes</h2>
+            <p></p>
+          </div>
+        </Container>
+      </div>
+    </AnimatedPage>
+  );
 };
 
 export default AllEliteNetworkView;
