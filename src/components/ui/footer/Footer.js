6b4eff6725966b1f81ed683c5662fda71294cb41
buraiyen <brian.edison.nguyen@gmail.com>
2022-07-11 17:24:57 -0700

    Footer: removed 'all rights reserved'

--- src/components/ui/footer/Footer.js
+++ src/components/ui/footer/Footer.js
@@ -6,15 +6,15 @@ import FooterButtons from './FooterButtons';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <Container maxWidth='xl'>
-        <div className='footer__block'>
+    <footer className="footer">
+      <Container maxWidth="xl">
+        <div className="footer__block">
           <Grid container spacing={3}>
             <Grid item xs={12} md={4}>
-              <div className='footer__section'>
+              <div className="footer__section">
                 <a
-                  href='mailto:brian.edison.nguyen@gmail.com'
-                  className='footer__link'
+                  href="mailto:brian.edison.nguyen@gmail.com"
+                  className="footer__link"
                 >
                   brian.edison.nguyen@gmail.com
                 </a>
@@ -24,19 +24,19 @@ const Footer = () => {
               <FooterBrand />
             </Grid>{' '}
             <Grid item xs={12} md={4}>
-              <div className='footer__section'>
+              <div className="footer__section">
                 <FooterButtons />
               </div>
             </Grid>
           </Grid>
         </div>
       </Container>
-      <hr className='footer__line-divider' />
-      <Container maxWidth='xs'>
-        <p className='footer__copyright'>
-          {new Date().getFullYear()} Brian Nguyen. All rights reserved
+      <hr className="footer__line-divider" />
+      <Container maxWidth="xs">
+        <p className="footer__copyright">
+          {new Date().getFullYear()} &mdash; Brian Nguyen
         </p>
-        <Link to='/privacy-policy' className='footer__privacy-policy-link'>
+        <Link to="/privacy-policy" className="footer__privacy-policy-link">
           Privacy Policy
         </Link>
       </Container>
