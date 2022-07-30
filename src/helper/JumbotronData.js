fd0941c2053d6c17c26bbd2942f7e37c8762a45a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-30 11:14:47 -0700

    JumbotronData: replaced 'image' with src

--- src/helper/JumbotronData.js
+++ src/helper/JumbotronData.js
@@ -1,46 +1,39 @@
 const HomeData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
   title: 'Brian E. Nguyen',
   description:
     '2020 Computer Science Graduate and Aspiring Front-end Developer',
 };
 
 const ResumeData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_laptop.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_laptop.jpg',
   title: 'Resume',
 };
 
 const PortfolioData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_code_hijtco.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_code_hijtco.jpg',
   title: 'Portfolio',
 };
 
 const AboutData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_legoart.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_legoart.jpg',
   title: 'About',
 };
 
 const BlogData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1620240499/BEN_Website/jumbotron/BEN_vegasstrip_vk4nx7.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1620240499/BEN_Website/jumbotron/BEN_vegasstrip_vk4nx7.jpg',
   title: 'My Blog',
   description: `Get insights into what I have been doing recently, whether it is for my career
     or my personal life`,
 };
 
 const ContactData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg',
   title: 'Contact',
 };
 
 const ErrorData = {
-  image:
-    'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg',
+  src: 'https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg',
   title: '404 Error',
   description: `The page that you are trying to reach does not or no longer exists. 
     If this is an issue, then feel free to contact me`,
