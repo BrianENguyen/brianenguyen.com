2ec459ae7dfd6ab297a83e6082162c13a2dcb4ee
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-11 10:54:22 -0700

    JumbotronData: change description for home view

--- src/helper/JumbotronData.js
+++ src/helper/JumbotronData.js
@@ -2,8 +2,7 @@ const HomeData = {
   image:
     'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915,b_black,o_70/v162020501/BEN_Website/jumbotron/BEN_coding.jpg',
   title: 'Brian E. Nguyen',
-  description:
-    '2020 Computer Science Graduate and Aspiring Front-end Developer',
+  description: 'Frontend Web Developer and Coding Instructor',
 };
 
 const ResumeData = {
