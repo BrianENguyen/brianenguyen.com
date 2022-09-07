15f1b0f4edc5994e15a47de707ecbe7b425300f6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-09-07 12:21:08 -0700

    fix: reworked jumbotron

--- src/components/ui/jumbotron/Jumbotron.js
+++ src/components/ui/jumbotron/Jumbotron.js
@@ -2,18 +2,24 @@ import './Jumbotron.css';
 
 const Jumbotron = (props) => {
   return (
-    <div
-      className='jumbotron'
-      style={{
-        backgroundImage: `url(${props.src})`,
-      }}
-    >
-      <div className='jumbotron__content'>
-        {props.logo}
-        <h1 className='jumbotron__title '>{props.title}</h1>
-        <p className='jumbotron__description'>{props.description}</p>
-      </div>
-    </div>
+     <div className="jumbotron_">
+        <picture>
+          <source src="jumboData.src" media="(min-width: 600px)" />
+          <img
+            src={props.src}
+            alt=""
+            className="jumbotron__image"
+            decoding="async"
+            rel="preload"
+          />
+        </picture>
+       <div className="jumbotron__container">
+         <h1 className="jumbotron__title">{props.title}</h1>
+         <p v-if="jumboData.description" className="jumbotron__description">
+           { props.description }
+         </p>
+       </div>
+     </div>
   );
 };
 
