dcbf8efc093194211e4725f6988fc888d3e09eed
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-09-08 12:41:32 -0700

    fix: optimized ProjectData images

--- src/helper/ProjectData.js
+++ src/helper/ProjectData.js
@@ -1,7 +1,7 @@
 const ProjectData = [
   {
     title: 'All Elite Network',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1651693922/BEN_Website/projects/aew-stage-fireworks.webp',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1651693922/BEN_Website/projects/aew-stage-fireworks.webp',
     link: '/all-elite-network',
     photoCredit: {
       name: 'All Elite Wrestling',
@@ -10,27 +10,27 @@ const ProjectData = [
   },
   {
     title: 'Frontend Mentor Challenges',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,e_brightness:85,w_1025/v1642187660/BEN_Website/projects/BEN_frontend.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1642187660/BEN_Website/projects/BEN_frontend.jpg',
     link: '/frontend-mentor-challenges',
   },
   {
     title: 'New Personal Website',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1723/v1639358227/BEN_Website/projects/BEN_3098_islkyr.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639358227/BEN_Website/projects/BEN_3098_islkyr.jpg',
     link: '/new-site',
   },
   {
     title: "Thu Nguyen's Website",
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1723/v1639518576/BEN_Website/recent_projects/thu.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639518576/BEN_Website/recent_projects/thu.jpg',
     link: '/thu-nguyen',
   },
   {
     title: 'CSULB Tzu Ching',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1443/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
     link: '/csulb-tzu-ching',
   },
   {
     title: 'YelpCamp',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1639529477/BEN_Website/projects/hugues-de-buyer_camp_fc2lzb.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639529477/BEN_Website/projects/hugues-de-buyer_camp_fc2lzb.jpg',
     link: '/yelpcamp',
     photoCredit: {
       name: 'Hugues de Buyer-Mimeure',
@@ -39,12 +39,12 @@ const ProjectData = [
   },
   {
     title: 'First Personal Website',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1639774676/BEN_Website/projects/ben_longexposure.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639774676/BEN_Website/projects/ben_longexposure.jpg',
     link: '/first-personal-site',
   },
   {
     title: 'Senior Project: Kraken Bracket',
-    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1639530141/BEN_Website/projects/florian-olivo_gaming_im9ugb.jpg',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639530141/BEN_Website/projects/florian-olivo_gaming_im9ugb.jpg',
     link: '/kraken-bracket',
     photoCredit: {
       name: 'Florian Olivo',
