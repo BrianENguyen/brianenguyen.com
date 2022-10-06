8c800cc68b4ec33ed64b419d8d94f919295189ef
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-10-06 12:22:52 -0700

    fix: explicit navbar brand height

--- src/components/ui/navbar/NavbarBrand.js
+++ src/components/ui/navbar/NavbarBrand.js
@@ -3,6 +3,7 @@ const NavbarBrand = (props) => {
     <img
       src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_110/v1662578878/BEN_Website/BEN-logo-navbar.webp'
       width={props.width}
+      height='100px'
       alt='BEN Logo'
     ></img>
   );
