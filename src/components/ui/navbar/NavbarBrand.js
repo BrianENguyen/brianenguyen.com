3861d68c14c58155cd60c6e363f179e26232a686
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-10-10 11:43:49 -0700

    fix: navbar logo positioning and navbar height

--- src/components/ui/navbar/NavbarBrand.js
+++ src/components/ui/navbar/NavbarBrand.js
@@ -1,11 +1,11 @@
 const NavbarBrand = (props) => {
   return (
     <img
-      src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_110/v1662578878/BEN_Website/BEN-logo-navbar.webp'
+      src='https:res.cloudinary.com/buraiyen/image/upload/c_scale,w_110/v1662578878/BEN_Website/BEN-logo-navbar.webp'
       width={props.width}
-      height='100px'
       alt='BEN Logo'
     ></img>
+    // <h1>{props.sizes.width}</h1>
   );
 };
 
