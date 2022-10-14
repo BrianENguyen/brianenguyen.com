1ea1bfcd5884241201d7329261492c8b7b0a5f40
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-10-14 12:31:58 -0700

    fix: logic on WIP blog

--- src/components/blog/BlogCard.js
+++ src/components/blog/BlogCard.js
@@ -5,13 +5,15 @@ const BlogCard = (props) => {
   return (
     <div className={classes['blog-card']}>
       <div className={classes['blog-card__main-link']}>
-        <Link to={props.link}>
+        <Link to={props.date === 'WIP' ? '#' : props.link}>
           <img
             src={props.image}
             className={classes['blog-card__image']}
             alt=''
           />
-          <h1 className={classes['blog-card__title']}>{props.title}</h1>
+          <h2 className={classes['blog-card__title']}>
+            {props.date === 'WIP' && <em>Coming soon:</em>} {props.title}
+          </h2>
         </Link>
       </div>
       <p className={classes['blog-card__description']}>{props.description}</p>
@@ -19,9 +21,11 @@ const BlogCard = (props) => {
         &#127751;{' '}
         <span className={classes['blog-card__date']}>{props.date}</span>
       </footer>
-      <Link to={props.link} className={classes['blog-card__link']}>
-        Read More
-      </Link>
+      {props.date !== 'WIP' &&
+        <Link to={props.link} className={classes['blog-card__link']}>
+          Read More
+        </Link>
+      }
     </div>
   );
 };
