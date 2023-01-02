dae631aa14a79ffaa2d1189388e0f50c076d088c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-02 11:10:31 -0800

    feat: introductory content for OI blog

--- src/views/blog/OptimizingImages.js
+++ src/views/blog/OptimizingImages.js
@@ -12,9 +12,25 @@ const WhyBlogView = () => {
   return (
     <AnimatedPage>
       <Container style={{ marginTop: 200 + 'px' }} maxWidth='md'>
-        <h1 className={classes['blog__title']}>Optmizing Images for Your Website</h1>
-        <div className={classes['blog__text']}>
-        </div>
+        <h1 className={classes['blog__title']}>
+          Optmizing Images for Your Website
+        </h1>
+        <section className={classes['blog__text']}>
+          <p className={classes['blog__date']}>2023</p>
+          <p>
+            Website images are one of the biggest culprits to slowing down your
+            web pages. In this blog, I discuss why images can slow down websites
+            and my ways to optimize my sites' images to improve load speeds
+          </p>
+        </section>
+        <section className={classes['blog__text']}>
+          <h2 className={classes['blog__header']}>Image Downloads</h2>
+          <p>
+            When you visit any page that has images, you, the client, send a
+            request to the server to retrieve information about the site's page.
+            This includes files such as HTML, CSS, JavaScript, images, etc.
+          </p>
+        </section>
       </Container>
     </AnimatedPage>
   );
