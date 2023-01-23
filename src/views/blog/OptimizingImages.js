009222cb283404e8def145fae2ac51318a800f67
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-22 16:54:47 -0800

    feat: lazy loading

--- src/views/blog/OptimizingImages.js
+++ src/views/blog/OptimizingImages.js
@@ -1,3 +1,5 @@
+import Highlight from 'react-highlight';
+
 import classes from './BlogView.module.css';
 import AnimatedPage from '../../components/animations/AnimatedPage';
 import { Container } from '@mui/material';
@@ -11,6 +13,10 @@ const WhyBlogView = () => {
 
   return (
     <AnimatedPage>
+      <link
+        rel='stylesheet'
+        href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-dark.min.css'
+      />
       <Container style={{ marginTop: 200 + 'px' }} maxWidth='md'>
         <h1 className={classes['blog__title']}>
           Optmizing Images for Your Website
@@ -47,8 +53,8 @@ const WhyBlogView = () => {
             online file compressors. A website that I recommend is{' '}
             <a href='https://compressor.io' target='_blank' rel='noreferrer'>
               compressor.io
-            </a>{' '}
-            It's a good idea to have a copy of the original, uncompressed file
+            </a>
+            . It's a good idea to have a copy of the original, uncompressed file
             in case you need any other uses for it.
           </p>
           <p>
@@ -56,6 +62,43 @@ const WhyBlogView = () => {
             <em>WebP</em> format is 26% smaller in size compared to PNG images
             while maintaining the same quality.
           </p>
+        </section>
+
+        <section className={classes['blog__text']}>
+          <h2 className={classes['blog__header']}>Lazy Loading Images</h2>
+          <p>
+            When you visit a page, any images that should appear on the page
+            will be downloaded from the server by default. This action is not a
+            big deal on pages that don't much content, but this is an issue on
+            pages that require scrolling, like this one.
+          </p>
+          <p>
+            Why is this an issue? Say you load up into a large page with many
+            contents. What if the moment you visit the page, you instead wanted
+            visit another one?
+            <em>
+              Every single image that should appear on the page is already
+              loaded, even if you didn't see it.{' '}
+            </em>
+            This is a waste of loading time from your end and a waste of server
+            resources
+          </p>
+          <p>
+            To prevent this issue, we want to only load images once our screen
+            is in view of where the image should be. This is where the concept
+            of <em>lazy loading</em> is used. Lazy loading using objects or
+            components only when they are needed.
+          </p>
+          <p>
+            To lazy load using the <code>{'<img />'}</code> HTML tag, you would
+            use an attribute called <code>loading</code> and set its value to{' '}
+            <code>lazy</code>
+          </p>
+          <div style={{ fontSize: '1.2rem' }}>
+            <Highlight language='html'>
+              {'<img src="example.webp" loading="lazy"/>'}
+            </Highlight>
+          </div>
         </section>
       </Container>
     </AnimatedPage>
