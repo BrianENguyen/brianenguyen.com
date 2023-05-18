0feb6b591c88b6deaf4bd97079526da4a846639c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-17 17:08:22 -0700

    :white_check_mark: test: improved AboutView test

--- src/views/main/About/AboutView.test.jsx
+++ src/views/main/About/AboutView.test.jsx
@@ -1,8 +1,11 @@
 import AboutView from './AboutView';
+import { render } from '@testing-library/react';
+
+global.console.warn = jest.fn();
 
 describe('AboutView', () => {
   test('renders text correctly', () => {
-    const { getTextBy } = render(<AboutView />);
-    expect(getTextBy('About Me')).toBeInTheDocument();
+    const { getByText } = render(<AboutView />);
+    expect(getByText(/about me/i).textContent).toEqual('About Me');
   });
 });
