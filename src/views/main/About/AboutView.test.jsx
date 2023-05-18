87b5dd47b1eda7f9a7a1df05464801fa2754b8a3
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-17 19:12:19 -0700

    :white_check_mark: test: AboutView snapshot

--- src/views/main/About/AboutView.test.jsx
+++ src/views/main/About/AboutView.test.jsx
@@ -1,11 +1,18 @@
 import AboutView from './AboutView';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 global.console.warn = jest.fn();
 
 describe('AboutView', () => {
   test('renders text correctly', () => {
     const { getByText } = render(<AboutView />);
-    expect(getByText(/about me/i).textContent).toEqual('About Me');
+    const jumboText = getByText(/about me/i);
+    expect(jumboText).toBeInTheDocument();
+  });
+
+  test('matches snapshot', () => {
+    const { asFragment } = render(<AboutView />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
