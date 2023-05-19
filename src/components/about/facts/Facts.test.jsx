934de14a98d2ba200c9ebfc2c9dbdb3515d17c9a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-18 21:55:27 -0700

    :white_check_mark: test: header rendering

--- src/components/about/facts/Facts.test.jsx
+++ src/components/about/facts/Facts.test.jsx
@@ -6,4 +6,13 @@ describe('Facts', () => {
   test('renders Facts component without any errors', () => {
     render(<Facts />);
   });
+
+  test('renders header and subheader correctly', () => {
+    const { getByText } = render(<Facts />);
+    const headerElement = getByText(/things that define me/i);
+    const subheaderElement = getByText(/i am a software developer/i);
+
+    expect(headerElement).toBeInTheDocument();
+    expect(subheaderElement).toBeInTheDocument();
+  });
 });
