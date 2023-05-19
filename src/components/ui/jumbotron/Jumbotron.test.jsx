770e36c97f44fb02dccdb5f7d56dd7296cc1fe61
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-19 16:49:36 -0700

    :white_check_mark: test: Jumbotron prop rendering

--- src/components/ui/jumbotron/Jumbotron.test.jsx
+++ src/components/ui/jumbotron/Jumbotron.test.jsx
@@ -6,4 +6,24 @@ describe('Jumbotron', () => {
   test('renders Jumbotron component without errors', () => {
     render(<Jumbotron />);
   });
+
+  test('renders correct elements and props', () => {
+    const props = {
+      title: 'Test Title',
+      description: 'Test Description',
+      src: 'test.webp',
+      srcMobile: 'test-mobile.webp',
+    };
+    render(<Jumbotron {...props} />);
+
+    const titleElement = screen.getByText(props.title);
+    const descriptionElement = screen.getByText(props.description);
+    const imageElement = screen.getByAltText('');
+
+    expect(titleElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement.src).toContain(props.srcMobile);
+    expect(imageElement).toHaveClass('jumbotron__image');
+  });
 });
