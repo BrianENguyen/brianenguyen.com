cc174de30e482726754aa196c3cff75420770d48
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-06 13:26:53 -0700

    :white_check_mark: test: FooterBrand render

--- src/components/ui/footer/__tests__/FooterBrand.test.jsx
+++ src/components/ui/footer/__tests__/FooterBrand.test.jsx
@@ -1,2 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import FooterBrand from '../FooterBrand';
+
+describe('FooterBrand test suite', () => {
+  test('renders FooterBrand component without errors', () => {
+    const { getByAltText } = render(<FooterBrand />);
+
+    const imgElement = getByAltText('BEN Logo');
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute(
+      'src',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_110/v1662578878/BEN_Website/BEN-logo-navbar.webp'
+    );
+    expect(imgElement).toHaveAttribute('width', '150px');
+    expect(imgElement).toHaveAttribute('height', '120px');
+    expect(imgElement).toHaveClass('footer__brand');
+  });
+});
