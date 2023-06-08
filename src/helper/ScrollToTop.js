62de35e8f6fb5f3da82cc82da13ead7f9e31dc26
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-08 14:06:32 -0700

    :memo: docs: ScrollToTop comments

--- src/helper/ScrollToTop.js
+++ src/helper/ScrollToTop.js
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
 
+/*
+Function used for scrolling to the top of the 
+page after the user navigates to a different 
+page via React Router
+*/
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
