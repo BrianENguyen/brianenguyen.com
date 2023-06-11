02d90bebe61e01ff18e222824c4631ebb505b6d7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-11 16:04:38 -0700

    :wrench: chore: update vite config for md

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import Markdown from 'vite-plugin-react-markdown';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Markdown(),
+    react({
+      include: [/\.tsx$/, /\.md$/], // <-- add .md
+    }),
+  ],
 });
