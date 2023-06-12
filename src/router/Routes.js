d25d4c59e76e10ffb97588ee376f114301cedee2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-12 14:05:21 -0700

    :sparkles: feat: titles in Routes

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -21,27 +21,39 @@ import AllEliteNetworkView from '../views/project/AllEliteNetworkView';
 // Blog views
 import BlogPostView from '../views/blog/BlogPostView';
 
+const suffix = '| BEN';
+
 const ROUTES = [
   {
     path: '/',
     key: 'Home',
     component: HomeView,
+    props: {
+      title: 'Brian E. Nguyen | Software Developer',
+    },
   },
   {
     path: '/portfolio',
     key: 'Portfolio',
     component: PortfolioView,
+    props: {
+      title: `Portfolio ${suffix}`,
+    },
   },
   {
     path: '/blog',
     key: 'Blog',
     component: BlogView,
+    props: {
+      title: `Blog ${suffix}`,
+    },
   },
   {
     path: '/blog/optimizing-images',
     key: 'Optimizing Images',
     component: BlogPostView,
     props: {
+      title: `Blog ${suffix}`,
       markdownFile: 'optimizing-images',
     },
   },
@@ -50,6 +62,7 @@ const ROUTES = [
     key: 'Why I Blog',
     component: BlogPostView,
     props: {
+      title: `Why I'm Blogging ${suffix}`,
       markdownFile: 'why-i-blog',
     },
   },
@@ -58,6 +71,7 @@ const ROUTES = [
     key: 'Volunteering with Tzu Chi',
     component: BlogPostView,
     props: {
+      title: `Volunteering with the Tzu Chi Foundation ${suffix}`,
       markdownFile: 'volunteering-tzu-chi',
     },
   },
@@ -66,6 +80,7 @@ const ROUTES = [
     key: 'Programming History',
     component: BlogPostView,
     props: {
+      title: `My Programming History ${suffix}`,
       markdownFile: 'programming-history',
     },
   },
@@ -73,66 +88,105 @@ const ROUTES = [
     path: '/about',
     key: 'About',
     component: AboutView,
+    props: {
+      title: `About ${suffix}`,
+    },
   },
   {
     path: '/contact',
     key: 'Contact',
     component: ContactView,
+    props: {
+      title: `Contact ${suffix}`,
+    },
   },
   {
     path: '/privacy-policy',
     key: 'Privacy Policy',
     component: PrivacyView,
+    props: {
+      title: `Privacy Policy ${suffix}`,
+    },
   },
   {
     path: '/portfolio/new-site',
     key: 'New Website',
     component: NewSiteView,
+    props: {
+      title: `My New Site ${suffix}`,
+    },
   },
   {
     path: '/portfolio/thu-nguyen',
     key: 'Thu Nguyen',
     component: ThuView,
+    props: {
+      title: `Thu Nguyen's Site ${suffix}`,
+    },
   },
   {
     path: '/portfolio/all-elite-network',
     key: 'All Elite Network',
     component: AllEliteNetworkView,
+    props: {
+      title: `All Elite Network ${suffix}`,
+    },
   },
   {
     path: '/portfolio/csulb-tzu-ching',
     key: 'CSULB Tzu Ching',
     component: LBTCView,
+    props: {
+      title: `CSULB Tzu Ching ${suffix}`,
+    },
   },
   {
     path: '/portfolio/yelpcamp',
     key: 'YelpCamp',
     component: YelpCamp,
+    props: {
+      title: `YelpCamp ${suffix}`,
+    },
   },
   {
     path: '/portfolio/first-personal-site',
     key: 'First Site',
     component: FirstSiteView,
+    props: {
+      title: `My First Site ${suffix}`,
+    },
   },
   {
     path: '/portfolio/kraken-bracket',
     key: 'Kraken Bracket',
     component: KrakenBracketView,
+    props: {
+      title: `Kraken Bracket ${suffix}`,
+    },
   },
   {
     path: '/portfolio/simd',
     key: 'SIMD',
     component: SIMDView,
+    props: {
+      title: `SIMD ${suffix}`,
+    },
   },
   {
     path: '/portfolio/probability-stats',
     key: 'Probability and Stats',
     component: ProbAndStatsView,
+    props: {
+      title: `Probability and Stats ${suffix}`,
+    },
   },
   {
     path: '*',
     key: 'Error',
     component: ErrorView,
+    props: {
+      title: `404 ${suffix}`,
+    },
   },
 ];
 
