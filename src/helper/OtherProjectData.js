823c5121fc94b0e72f6b2ede098c34ed3865ee8b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-12 17:06:56 -0700

    :bug: fix: other project links

--- src/helper/OtherProjectData.js
+++ src/helper/OtherProjectData.js
@@ -1,7 +1,7 @@
 // Data used for OtherProjectsList components
 const OtherProjectData = [
-  { title: 'SIMD Instructions', link: '/simd' },
-  { title: 'Probability and Stats', link: '/probability-stats' },
+  { title: 'SIMD Instructions', link: '/portfolio/simd' },
+  { title: 'Probability and Stats', link: '/portfolio/probability-stats' },
 ];
 
 export default OtherProjectData;
