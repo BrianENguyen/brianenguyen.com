81300dc398b2b134eba39672c341d0b8406672c9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-15 13:39:35 -0700

    :lipstick: style: passion image animations

--- src/components/home/Passion.jsx
+++ src/components/home/Passion.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Grid, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Button from '../ui/button/Button';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './Passion.css';
+import '../animations/animations.css';
+
 const Passion = () => {
+  useEffect(() => {
+    AOS.init({ duration: 3000 });
+  }, []);
+
   return (
     <div className='passion'>
       <Container>
         <Grid container spacing={2}>
-          <Grid item sm={12} md={5}>
+          <Grid
+            item
+            sm={12}
+            md={5}
+            data-aos='reveal-left'
+            data-aos-offset='250'
+            data-aos-once='true'
+          >
             <img
               src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_450/v1675707556/BEN_Website/BEN_profile3.webp'
               width='450px'
