f47fd1f5359597d0d9bd7539a1f99181995ffc00
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-15 20:58:02 -0700

    :lipstick: style: blog card animation

--- src/components/blog/blog-card/BlogCard.jsx
+++ src/components/blog/blog-card/BlogCard.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';
 import classes from './BlogCard.module.css';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import '../../animations/animations.css';
+import 'aos/dist/aos.css';
 
 /**
  * The blog card can be active or disabled depending on if it's
@@ -14,34 +17,55 @@ import { Link } from 'react-router-dom';
  * 'Read More' link won't appear
  * */
 const BlogCard = (props) => {
+  useEffect(() => {
+    AOS.init({ duration: 3000 });
+  }, []);
+
   return (
     <div className={classes['blog-card']} data-testid='blog-card'>
       <div className={classes['blog-card__main-link']}>
         <Link to={props.date === 'WIP' ? '#' : props.link}>
-          <img
-            src={props.image}
-            lazy-src={props.image}
-            className={classes['blog-card__image']}
-            decoding='async'
-            rel='preload'
-            loading='lazy'
-            alt=''
-          />
-          <h2 className={classes['blog-card__title']}>
+          <div
+            data-aos='reveal-down'
+            data-aos-offset='250'
+            data-aos-once='true'
+          >
+            <img
+              src={props.image}
+              lazy-src={props.image}
+              className={classes['blog-card__image']}
+              decoding='async'
+              rel='preload'
+              loading='lazy'
+              alt=''
+            />
+          </div>
+          <h2
+            className={classes['blog-card__title']}
+            data-aos='reveal-left'
+            data-aos-once='true'
+          >
             {props.date === 'WIP' && <em>Coming soon:</em>} {props.title}
           </h2>
         </Link>
       </div>
-      <p className={classes['blog-card__description']}>{props.description}</p>
-      <footer>
-        &#127751;{' '}
-        <span className={classes['blog-card__date']}>{props.date}</span>
-      </footer>
-      {props.date !== 'WIP' && (
-        <Link to={props.link} className={classes['blog-card__link']}>
-          Read More
-        </Link>
-      )}
+      <div
+        data-aos='fade-up'
+        data-aos-offset='250'
+        data-aos-once='true'
+        data-aos-duration='1000'
+      >
+        <p className={classes['blog-card__description']}>{props.description}</p>
+        <footer>
+          &#127751;{' '}
+          <span className={classes['blog-card__date']}>{props.date}</span>
+        </footer>
+        {props.date !== 'WIP' && (
+          <Link to={props.link} className={classes['blog-card__link']}>
+            Read More
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
