4aee562d3148dba8533152fac17c08cbf7d99cfc
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-06-15 21:06:29 -0700

    :lipstick: style: jumbotron animations

--- src/components/ui/jumbotron/Jumbotron.jsx
+++ src/components/ui/jumbotron/Jumbotron.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import '../../animations/animations.css';
 import './Jumbotron.css';
 
 const Jumbotron = (props) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className='jumbotron'>
       <picture>
@@ -16,8 +24,22 @@ const Jumbotron = (props) => {
         />
       </picture>
       <div className='jumbotron__container'>
-        <h1 className='jumbotron__title'>{props.title}</h1>
-        <p className='jumbotron__description'>{props.description}</p>
+        <h1
+          className='jumbotron__title'
+          data-aos='fade-up'
+          data-aos-once='true'
+          data-aos-duration='1000'
+        >
+          {props.title}
+        </h1>
+        <p
+          className='jumbotron__description'
+          data-aos='fade-up'
+          data-aos-once='true'
+          data-aos-duration='1000'
+        >
+          {props.description}
+        </p>
       </div>
     </div>
   );
