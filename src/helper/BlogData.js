2906a645b119a53418424e84da826410b80bcbc2
buraiyen <brian.edison.nguyen@gmail.com>
2023-07-04 16:21:47 -0700

    :sparkles: feat: universal blog thumbnail

--- src/helper/BlogData.js
+++ src/helper/BlogData.js
@@ -1,11 +1,20 @@
 // Data used for BlogList component to render blogs
 const BlogData = [
   {
+    title: 'Visiting Universal Studios Hollywood After 10+ Years',
+    description: `This blog documents my recent experience at Universal Studios Hollywood and my opinions on how much the park 
+    transformed the past decade.`,
+    image:
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1665774712/BEN_Website/blog/Universal/BEN_universal_neon_sign.webp',
+    date: '2023-07-04',
+    link: '/blog/universal-studios',
+  },
+  {
     title: 'My First Maternity Photography Session',
     description: `I've provided my first photography session for a couple who is expecting a baby. This 
     blog showcases various photos and how I put together this session`,
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1665774712/BEN_Website/blog/Maternity/BEN_maternity2.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1665774712/BEN_Website/blog/Maternity/BEN_maternity2.webp',
     date: '2023-06-30',
     link: '/blog/maternity-photos',
   },
@@ -14,7 +23,7 @@ const BlogData = [
     description: `I visited Nebraska for a business trip, now I'm moving there in July! I discuss the events that led
     up to my relocation, how moving there will benefit me, and my experiences in Nebraska`,
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1665774712/BEN_Website/blog/BEN_road.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1665774712/BEN_Website/blog/BEN_road.webp',
     date: '2023-06-15',
     link: '/blog/hello-nebraska',
   },
@@ -23,7 +32,7 @@ const BlogData = [
     description: `I switched from using JSX to Org Mode for writing my blogs. Discover the reasons behind this transition,
      aiming for a smoother and more enjoyable blogging experience.`,
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1665774712/BEN_Website/blog/BEN_org.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1665774712/BEN_Website/blog/BEN_org.webp',
     date: '2023-06-12',
     link: '/blog/blogging-with-org-mode',
   },
@@ -32,7 +41,7 @@ const BlogData = [
     description: `Images on your website are one of the biggest culprits to slowing down your website.
        In this blog, I will discuss how I optimize my website's images to improve load speed.`,
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1665774712/BEN_Website/blog/OptimizingImages/BEN_photo-files.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1665774712/BEN_Website/blog/OptimizingImages/BEN_photo-files.webp',
     date: '2023-01-23',
     link: '/blog/optimizing-images',
   },
@@ -43,7 +52,7 @@ const BlogData = [
       how CSULB Tzu Ching went through its worse times, and how I became president of CSULB Tzu Ching to lead
       the organization by myself.`,
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1644820389/BEN_Website/blog/BEN_orientation.jpg ',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1644820389/BEN_Website/blog/BEN_orientation.jpg ',
     date: '2022-02-16',
     link: '/blog/volunteering-with-tzu-chi',
   },
@@ -52,7 +61,7 @@ const BlogData = [
     description:
       'Learn about my programming history, from when I started in high school, up until now.',
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1644628220/BEN_Website/blog/BEN_4121_1_jw8m5b.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1644628220/BEN_Website/blog/BEN_4121_1_jw8m5b.jpg',
     date: '2022-02-12',
     link: '/blog/programming-history',
   },
@@ -60,7 +69,7 @@ const BlogData = [
     title: "Why I'm Blogging",
     description: "Recently, I decided that I will start blogging. Here's why.",
     image:
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_300/v1642186176/BEN_Website/blog/BEN_redondobeach.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_800/v1642186176/BEN_Website/blog/BEN_redondobeach.jpg',
     date: '2022-02-11',
     link: '/blog/why-i-blog',
   },
