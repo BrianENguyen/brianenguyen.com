3586f0019417cd936a1f8043e96e5c35155aafdc
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-20 20:27:15 -0700

    :lipstick: style: global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@unocss/nuxt'],
   components: [{ path: '~/components', extensions: ['.vue'] }],
+  css: ['assets/styles/global.css'],
 });
