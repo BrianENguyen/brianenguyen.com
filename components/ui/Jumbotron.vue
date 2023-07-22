82c27726bcaee9593fd2d13b010b27158a122171
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-22 08:37:45 -0700

    :lipstick: style: jumbotron min-0 width

--- components/ui/Jumbotron.vue
+++ components/ui/Jumbotron.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="text-center relative bg-black border-none">
+  <div
+    class="relative
+        bg-black
+          border-none 
+          lg:h-screen
+          zero:mt-[75px]
+          zero:h-[35vh]
+          xs:mt-[85px]
+          xs:h-[50vh]"
+  >
     <picture>
       <source :srcset="src || ''" media="(min-width: 600px)" />
       <img
@@ -7,12 +16,14 @@
         :lazy-src="srcMobile || ''"
         alt=""
         decoding="async"
-        class="object-cover w-screen relative z-0"
+        :class="srcMobile && 'object-cover w-screen relative z-0 zero:h-[35vh] xs:h-[50vh]'"
       />
     </picture>
-    <div class="absolute top-[50%] left-[50%]">
-      <h1 class="text-white">{{ jumbotronTitle }}</h1>
-      <p class="text-white">{{ jumbotronDesc }}</p>
+    <div
+      class="text-white absolute inset-0 flex justify-center items-center flex-col"
+    >
+      <h1 class="zero:text-[7vw] xs:text-[5vw]">{{ jumbotronTitle }}</h1>
+      <h1 class="zero:text-[1.2rem]">{{ jumbotronDesc }}</h1>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       :jumbotronTitle="'Brian E. Nguyen'"
       :jumbotronDesc="'Web Developer'"
       :src="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_developing.webp'"
+      :srcMobile="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_developing.webp'"
     />
   </div>
 </template>
