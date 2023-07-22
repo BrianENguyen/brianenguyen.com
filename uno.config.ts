f99d40b02c34865c7c7cbb1a699e01a5712b44f6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-21 21:41:08 -0700

    :wrench: chore: breakpoint configs

--- uno.config.ts
+++ uno.config.ts
@@ -2,5 +2,13 @@ import { defineConfig, presetIcons, presetUno } from 'unocss';
 
 export default defineConfig({
   rules: [[/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })]],
+  theme: {
+    breakpoints: {
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+    },
+  },
   presets: [presetIcons({ prefix: 'i-' }), presetUno()],
 });
