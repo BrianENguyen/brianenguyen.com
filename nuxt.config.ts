50f3eb24e4543d8d668b400ac3b1a9e050707bdb
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-21 17:04:00 -0700

    :heavy_plus_sign: chore: installed unocss reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@unocss/nuxt'],
   components: [{ path: '~/components', extensions: ['.vue'] }],
-  css: ['assets/styles/global.css'],
+  css: ['assets/styles/global.css', '@unocss/reset/eric-meyer.css'],
 });
