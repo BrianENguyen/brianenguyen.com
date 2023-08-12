caa5ef1b5c9c6f221234fa663f9db00203dfebc9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-12 11:55:26 -0500

    :sparkles: init: default layout

---
+++ components/index/Passion.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h1>Gigatummy</h1>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,18 @@
       :src="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_developing.webp'"
       :srcMobile="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_developing.webp'"
     />
+    <Passion />
   </div>
 </template>
 
 <script>
 import Jumbotron from '~/components/ui/Jumbotron.vue';
+import Passion from '~/components/index/Passion.vue';
 
 export default {
   components: {
-    Jumbotron
+    Jumbotron,
+    Passion
   }
 }
 </script>
