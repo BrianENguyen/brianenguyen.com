ef435908763e2ca9aac738aba474270f94b30145
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-13 12:00:16 -0500

    :sparkles: feat: about me jumbotron

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <Jumbotron
+    :jumbotronTitle="'About Me'"
+    :src="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
+    :srcMobile="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
+  />
+</template>
+
+<script>
+import Jumbotron from '~/components/ui/Jumbotron.vue'
+export default {
+  components: {
+    Jumbotron
+  }
+}
+</script>
