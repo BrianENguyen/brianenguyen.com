12dc78ba7f17f33816dcc944bbca88ef9685f134
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-13 12:06:26 -0500

    :sparkles: feat: Backstory

---
+++ components/about/Backstory.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="grid xs:grid-cols-1 md:grid-cols-2">
+    <div>
+      <h2 class="text-4xl">My Story</h2>
+    </div>
+    <div>
+      <p>
+        I obtained a Bachelor's in Computer Science from California State
+        University, Long Beach in 2020. From the moment I wrote my first Python
+        script in my introductory CS course, I knew that coding is something I
+        wanted to do in my life. It has allowed me to challenge myself and
+        improve my skills, not just in programming, but also in creativity and
+        team-working. Furthermore, I am grateful to have learned a myriad of
+        technologies and concepts, and have connected with many people who have
+        similar goals.
+      </p>
+      <p>
+        I currently work at IdeaBank Marketing as a web developer. This role
+        allows me to work in a team to develop high-quality websites for clients
+        that will not only fulfill but also exceed their expectations
+      </p>
+      <p>
+        I formerly worked at Code Ninjas, Cerritos and Fullterton, as a coding
+        instructor and an assistant director. Working here combines two of my
+        passions: programming and working with students. I have tutored students
+        with JavaScript, Lua, and C# languages to help them build high-quality
+        software. I have also taught web development at various district schools
+        to over 400 students.
+      </p>
+      <p>
+        Apart from my programming passions, I formerly served as the president
+        of the Tzu Chi Collegiate Association at CSULB. This volunteering
+        organization focuses on humanitarian aid, medicine, education, and more.
+        I have devoted over 300 hours of my time to serving this organization
+        and it has been a life-changing experience for me.
+      </p>
+    </div>
+  </section>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -4,13 +4,21 @@
     :src="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
     :srcMobile="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
   />
+  <DefaultLayout>
+    <Backstory />
+  </DefaultLayout>
 </template>
 
 <script>
-import Jumbotron from '~/components/ui/Jumbotron.vue'
+import Jumbotron from '~/components/ui/Jumbotron'
+import DefaultLayout from '~/layouts/default'
+import Backstory from '~/components/about/Backstory'
+
 export default {
   components: {
-    Jumbotron
+    Jumbotron,
+    DefaultLayout,
+    Backstory
   }
 }
 </script>
