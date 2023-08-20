e7fa0e1e6d0e559b93c610010af65d804f1272d2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-20 12:41:32 -0500

    :sparkles: feat: TheGallery images

--- components/about/TheGallery.vue
+++ components/about/TheGallery.vue
@@ -3,10 +3,9 @@
   This is called "The Gallery" because each photo 
   starts with the word "The" 
   -->
-
-  <section class="grid grid-cols-4">
+  <section class="grid grid-cols-4 gap-8">
     <div v-for="image in images" :key="image.src">
-      <img :src="image.src" alt="" class="w-[100%] rounded" />
+      <img :src="image.src" :alt="image.alt" class="w-[100%] rounded" />
       <p class="text-center text-xl">{{ image.title }}</p>
     </div>
   </section>
@@ -20,8 +19,24 @@ export default {
       images: [
         {
           src: `${urlPrefix}/BEN_me_and_thu.webp`,
+          alt: "Thu hugging me while she is sitting down",
           title: "The wife"
-        }
+        },
+        {
+          src: `${urlPrefix}/BEN_me_and_mimi.webp`,
+          alt: "Me sitting with Mimi, kissing her forehead",
+          title: "The best dog ever"
+        },
+        {
+          src: `${urlPrefix}/BEN_me_kiki_and_lucky.webp`,
+          alt: "Me sitting with Kiki and Lucky",
+          title: "The two other best dogs ever"
+        },
+        {
+          src: `${urlPrefix}/BEN_org.webp`,
+          alt: "Org mode workflow",
+          title: "The focus"
+        },
       ]
     }
   }
