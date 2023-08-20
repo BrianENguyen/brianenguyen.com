0a3df4fcb39444cb9ced04860f43d749c86c922f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-20 15:54:13 -0500

    :sparkles: feat: SectionHeader

---
+++ components/ui/SectionHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="['mb-4', {'text-center': center}]">
+    <h2 class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0">
+      {{ title }}
+    </h2>
+    <p class="text-4xl font-[Playfair_Display] mt-0 mb-4">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'center']
+}
+</script>
