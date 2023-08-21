609f39bd88d5d5ce7e90c4591ccbd6e8daed37e6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-20 20:29:34 -0500

    :sparkles: init: resume

---
+++ pages/resume.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Jumbotron
+      jumbotronTitle="My Resume"
+      src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
+      srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
+    />
+  </div>
+</template>
+
+<script>
+import Jumbotron from '~/components/ui/Jumbotron.vue';
+export default {
+  components: {
+    Jumbotron,
+  }
+}
+</script>
