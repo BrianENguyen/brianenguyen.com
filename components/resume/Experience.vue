5c2917b185a02ed46fb834a468dbf13620f4783f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-20 21:17:06 -0500

    :sparkles: feat: experience idb

---
+++ components/resume/Experience.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="grid zero:grid-cols-1 md:grid-cols-5">
+    <h2 class="text-4xl mt-0 col-span-2">Experience</h2>
+    <div class="col-span-3">
+      <h3
+        class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0"
+      >
+        Web Developer
+      </h3>
+      <p class="text-2xl font-[Playfair_Display]">
+        <a href="https://ideabankmarketing.com" target="_blank">
+          IdeaBank Marketing </a
+        >. Hastings, NE, July 2023 - Present
+      </p>
+      <p>
+        Worked closely with clients to develop websites, which not only met
+        their expectations, but to also exceeded them. Developed projects with
+        varying sizes. Worked with the UI/UX designer to bring website designs
+        to life. Primary technologies included HTML, CSS3, JavaScript, jQuery,
+        PHP, and MODX.
+      </p>
+    </div>
+  </section>
+</template>
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <Jumbotron
-      jumbotronTitle="My Resume"
-      src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
-      srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
-    />
-  </div>
+  <Jumbotron
+    jumbotronTitle="My Resume"
+    src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
+    srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
+  />
+  <NuxtLayout>
+    <Experience />
+  </NuxtLayout>
 </template>
 
 <script>
-import Jumbotron from '~/components/ui/Jumbotron.vue';
+import Jumbotron from '~/components/ui/Jumbotron';
+import Experience from '~/components/resume/Experience';
+
 export default {
   components: {
     Jumbotron,
+    Experience
   }
 }
 </script>
