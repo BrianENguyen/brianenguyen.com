289525bee530ea30109b3ddafe581b138171d672
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-25 21:17:03 -0500

    :sparkles: init: resume education

---
+++ components/resume/Education.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="grid zero:grid-cols-1 md:grid-cols-5">
+    <h2 class="text-4xl mt-0 col-span-2">Education</h2>
+    <div class="col-span-3">
+      <div>
+        <h3
+          class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0"
+        >
+          Bachelor's Degree in Computer Science
+        </h3>
+        <p class="text-2xl font-[Playfair_Display]">
+          California State University, Long Beach, August 2016- May 2020
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
