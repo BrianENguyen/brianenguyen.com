6871dda5b400d6bb28a52f0867df2d8851a2dcd8
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-02 14:42:30 -0500

    :speech_balloon: chore: resume cn

--- components/resume/Experience.vue
+++ components/resume/Experience.vue
@@ -1,24 +1,46 @@
 <template>
   <section class="grid zero:grid-cols-1 md:grid-cols-5">
-    <h2 class="text-4xl mt-0 col-span-2">Experience</h2>
+    <div class="col-span-2">
+      <h2 class="text-4xl mt-0">Experience</h2>
+    </div>
     <div class="col-span-3">
-      <h3
-        class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0"
-      >
-        Web Developer
-      </h3>
-      <p class="text-2xl font-[Playfair_Display]">
-        <a href="https://ideabankmarketing.com" target="_blank">
-          IdeaBank Marketing </a
-        >. Hastings, NE, July 2023 - Present
-      </p>
-      <p>
-        Worked closely with clients to develop websites, which not only met
-        their expectations, but to also exceeded them. Developed projects with
-        varying sizes. Worked with the UI/UX designer to bring website designs
-        to life. Primary technologies included HTML, CSS3, JavaScript, jQuery,
-        PHP, Wordpress, and MODX.
-      </p>
+      <div class="mb-12">
+        <h3
+          class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0"
+        >
+          Web Developer
+        </h3>
+        <p class="text-2xl font-[Playfair_Display]">
+          <a href="https://ideabankmarketing.com" target="_blank">
+            IdeaBank Marketing </a
+          >. Hastings, NE, July 2023 - Present
+        </p>
+        <p>
+          Worked closely with clients to develop websites, which not only met
+          their expectations, but to also exceeded them. Developed projects with
+          varying sizes. Worked with the UI/UX designer to bring website designs
+          to life. Primary technologies included HTML, CSS3, JavaScript, jQuery,
+          PHP, Wordpress, and MODX.
+        </p>
+      </div>
+      <div class="mb-8">
+        <h3
+          class="text-brian-blue uppercase text-sm font-bold font-[Poppins] mb-0"
+        >
+          Assistant Director
+        </h3>
+        <p class="text-2xl font-[Playfair_Display]">
+          <a href="https://www.codeninjas.com/ca-cerritos" target="_blank">
+            Code Ninjas </a
+          >. Cerritos and Fullerton, CA, August 2020 - June 2023
+        </p>
+        <p>
+          Administered web development courses to 150+ middle and high school
+          students, offering guidance in JavaScript, Lua, and C#. Provided
+          valuable assistance, leading to a 50% increase in completion rates
+          among students in the computer science curriculum.
+        </p>
+      </div>
     </div>
   </section>
 </template>
