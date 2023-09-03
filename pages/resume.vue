a8eabfd2758c7e90106ea317ff8b8301de0244ca
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-02 20:20:23 -0500

    :sparkles: feat: import resume components

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,22 +1,13 @@
 <template>
-  <Jumbotron
+  <UiJumbotron
     jumbotronTitle="My Resume"
     src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
     srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_thinkpad.webp"
   />
   <NuxtLayout>
-    <Experience />
+    <ResumeEducation />
+    <ResumeExperience />
+    <ResumeProjects />
+    <ResumeSkills />
   </NuxtLayout>
 </template>
-
-<script>
-import Jumbotron from '~/components/ui/Jumbotron';
-import Experience from '~/components/resume/Experience';
-
-export default {
-  components: {
-    Jumbotron,
-    Experience
-  }
-}
-</script>
