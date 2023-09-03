b676ffef1f33cb627bcfccc514f4946d448c0acb
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-02 20:05:48 -0500

    :sparkles: feat: resume skills

---
+++ components/resume/Skills.vue
@@ -0,0 +1,38 @@
+<template>
+  <hr />
+  <section class="grid zero:grid-cols-1 md:grid-cols-5">
+    <div class="col-span-2">
+      <h2 class="text-4xl mt-0 col-span-2">Skills</h2>
+    </div>
+    <div class="col-span-3 grid zero:grid-cols-1 xs:grid-cols-3">
+      <ul>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JavaScript ES6</li>
+        <li>C#</li>
+        <li>Python</li>
+        <li>SQL</li>
+      </ul>
+      <ul>
+        <li>React</li>
+        <li>Vue / Nuxt</li>
+        <li>Redux</li>
+        <li>Express</li>
+        <li>Tailwind / UnoCSS</li>
+        <li>SCSS</li>
+        <li>Node</li>
+        <li>.NET Framework</li>
+      </ul>
+      <ul>
+        <li>Linux</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>Jest</li>
+        <li>Cypress</li>
+        <li>Adobe XD</li>
+        <li>PenPot</li>
+        <li>Cloudinary</li>
+      </ul>
+    </div>
+  </section>
+</template>
