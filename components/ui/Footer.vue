59efd62c8836ab9e11d84424ff2024d1c1b83df0
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-04 10:59:33 -0500

    :sparkles: feat: 1st footer section

--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -1,3 +1,39 @@
 <template>
-  <footer class="bg-[#171717] text-center border-t-red border-solid"></footer>
+  <footer
+    class="bg-[#171717] text-center border-t-gray-light border-t-solid border-t-1 mt-8"
+  >
+    <div class="mx-auto px-4 py-5">
+      <div class="grid grid-cols-3 gap-4 items-center align-center">
+        <!-- Email -->
+        <div>
+          <a href="mailto:brian.edison.nguyen@gmail.com"
+            >brian.edison.nguyen@gmail.com</a
+          >
+        </div>
+        <!-- Brand -->
+        <div>
+          <img
+            src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_110/v1662578878/BEN_Website/BEN-logo-navbar.webp"
+            width="150px"
+            height="100px"
+            class="w-150px h-100px"
+            loading="lazy"
+            alt="BEN Logo"
+          />
+        </div>
+        <!-- Icons -->
+        <div class="flex">
+          <a href="https://www.linkedin.com/in/brian-e-nguyen/" target="_blank">
+            <div class="i-bxl-linkedin-square text-5xl mx-2" />
+          </a>
+          <a href="https://github.com/Buraiyen/" target="_blank">
+            <div class="i-bxl-github text-5xl mx-2" />
+          </a>
+          <a href="https://www.instagram.com/buraiyen_" target="_blank">
+            <div class="i-bxl-instagram text-5xl mx-2" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
