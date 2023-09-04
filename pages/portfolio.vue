00df705ca575eded16ad01e1e12dd5b36e7e17d9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-04 12:52:27 -0500

    :sparkles: init: portfolio

---
+++ pages/portfolio.vue
@@ -0,0 +1,7 @@
+<template>
+  <UiJumbotron
+    jumbotronTitle="Portfolio"
+    src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
+    srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
+  />
+</template>
