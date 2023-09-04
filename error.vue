78b3879f561d1cf26a7c969c8d41bcd4c4e1e084
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-04 09:42:43 -0500

    :recycle: chore: error view fragment warning

--- error.vue
+++ error.vue
@@ -20,37 +20,39 @@ fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
 </script>
 
 <template>
-  <UiNavbar />
-  <UiJumbotron
-    jumbotronTitle="404 Error"
-    jumbotronDesc="The page that you are trying to reach does not exist or is no longer available."
-    src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg"
-    srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg"
-  />
+  <div>
+    <UiNavbar />
+    <UiJumbotron
+      jumbotronTitle="404 Error"
+      jumbotronDesc="The page that you are trying to reach does not exist or is no longer available."
+      src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg"
+      srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1620240515/BEN_Website/BEN_huntingtonbeach_ac9rzj.jpg"
+    />
 
-  <NuxtLayout class="text-lg">
-    <p>
-      If this is a problem, please don't hesitate to
-      <a href="mailto:brian.edison.nguyen@gmail.com">email me</a>, or
-      <a
-        href="https://github.com/Buraiyen/brianenguyen.com/issues"
-        target="_blank"
-      >
-        send an issue on GitHub.
-      </a>
-    </p>
-    <p>While you're here, here's a random joke:</p>
+    <NuxtLayout class="text-lg">
+      <p>
+        If this is a problem, please don't hesitate to
+        <a href="mailto:brian.edison.nguyen@gmail.com">email me</a>, or
+        <a
+          href="https://github.com/Buraiyen/brianenguyen.com/issues"
+          target="_blank"
+        >
+          send an issue on GitHub.
+        </a>
+      </p>
+      <p>While you're here, here's a random joke:</p>
 
-    <!-- 
+      <!-- 
       For some reason the API doesn't always return the joke,
       so I just hardcoded one just in case
     -->
-    <div v-if="!loading">
-      <p v-if="joke.setup">{{ joke.setup }}</p>
-      <p v-else>Why did the programmer quit his job?</p>
-      <p v-if="joke.delivery">{{ joke.delivery }}</p>
-      <p v-else>Because he didn't get arrays.</p>
-    </div>
-    <NuxtLink to="/">Go back home</NuxtLink>
-  </NuxtLayout>
+      <div v-if="!loading">
+        <p v-if="joke.setup">{{ joke.setup }}</p>
+        <p v-else>Why did the programmer quit his job?</p>
+        <p v-if="joke.delivery">{{ joke.delivery }}</p>
+        <p v-else>Because he didn't get arrays.</p>
+      </div>
+      <NuxtLink to="/">Go back home</NuxtLink>
+    </NuxtLayout>
+  </div>
 </template>
