f4712abf4bfa78383074b08ae8d981821bc1cb0a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-04 14:25:24 -0500

    :sparkles: feat: portfolio page

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,14 +1,24 @@
+<script setup>
+const route = useRoute()
+console.log(route.path)
+</script>
+
 <template>
-  <UiJumbotron
-    jumbotronTitle="Portfolio"
-    src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
-    srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
-  />
-  <NuxtLayout>
-    <UiSectionHeader
-      title="My work"
-      description="A collection of various websites that I have designed and developed"
+  <div v-if="route.path === '/portfolio/'">
+    <UiJumbotron
+      jumbotronTitle="Portfolio"
+      src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
+      srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1659204060/BEN_Website/jumbotron/BEN_webdev_code.webp"
     />
-    <PortfolioProjectList />
+    <NuxtLayout>
+      <UiSectionHeader
+        title="My work"
+        description="A collection of various websites that I have designed and developed"
+      />
+      <PortfolioProjectList />
+    </NuxtLayout>
+  </div>
+  <NuxtLayout :name="'project'" v-else>
+    <NuxtPage />
   </NuxtLayout>
 </template>
