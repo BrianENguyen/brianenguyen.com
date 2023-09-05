c9cdb9be556ada3c14c552744e26647600bf738d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-04 19:11:28 -0500

    :lipstick: style: more nuxt-content

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="nuxt-content mt-48 mx-auto max-w-900px h-[100vh] px-4">
+  <article class="nuxt-content mt-48 mx-auto max-w-900px min-h-[100vh] px-4">
     <ContentDoc />
   </article>
 </template>
