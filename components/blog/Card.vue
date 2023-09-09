0a40f76a4b66e8226f955bad193f6eaa291e31ae
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-09 14:50:57 -0500

    :sparkles: feat: blog card and blog list

--- components/blog/Card.vue
+++ components/blog/Card.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Hi</h1>
+  <div>
+    <NuxtLink :to="blog.link" class="!text-white decoration-none">
+      <img
+        :src="blog.image"
+        class="w-[100%] max-w-350px h-auto max-h-450px rounded"
+      />
+      <h2>{{ blog.title }}</h2>
+    </NuxtLink>
+    <p>{{ blog.description }}</p>
+    <div>
+      <p>
+        &#127751; <em>{{ blog.date }}</em>
+      </p>
+      <NuxtLink :to="blog.link">Read More </NuxtLink>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['blog']
+}
+</script>
--- components/blog/List.vue
+++ components/blog/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="grid zero:grid-cols-1 md:grid-cols-3 gap-4">
+  <section class="grid zero:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
     <BlogCard v-for="blog in blogList" :key="blog.title" :blog="blog" />
   </section>
 </template>
