a00991006f13f2afbdb87f0950d708cf8aaa2644
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-09 20:01:53 -0500

    :recycle: ref: reworked prose

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -1,9 +1,5 @@
 <template>
-  <a
-    class="inline-block mx-2 my-4 border-brian-blue border-1 border-solid p-4 rounded decoration-none"
-    :href="href"
-    target="_blank"
-  >
+  <a :href="href" target="_blank">
     <slot />
   </a>
 </template>
