8b2f47b8c28e3924d26dda21753b705bbda4cd3c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-22 23:19:33 -0500

    :fire: chore: removed layout import

--- pages/about.vue
+++ pages/about.vue
@@ -4,22 +4,12 @@
     :src="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
     :srcMobile="'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1659204062/BEN_Website/jumbotron/BEN_lego.webp'"
   />
-  <DefaultLayout>
+  <NuxtLayout>
     <AboutQuote />
     <AboutFacts />
     <AboutBackstory />
     <AboutTheGallery />
     <AboutPhotoGallery />
     <AboutMoreToExplore />
-  </DefaultLayout>
+  </NuxtLayout>
 </template>
-
-<script>
-import DefaultLayout from '~/layouts/default'
-
-export default {
-  components: {
-    DefaultLayout
-  }
-}
-</script>
