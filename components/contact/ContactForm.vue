56fc597cfad838e69876cbc565121b56b25b1eda
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-23 12:04:09 -0500

    :sparkles: feat: init form submit handler, math

--- components/contact/ContactForm.vue
+++ components/contact/ContactForm.vue
@@ -7,13 +7,14 @@
     your email address is correct. I'll do my best to respond to you as soon as
     I can!
   </p>
-  <form>
+  <form @submit.prevent="handleSubmit" class="mx-4">
     <div class="grid gap-[3rem] zero:grid-cols-1 md:grid-cols-2 mb-4">
       <div>
         <label for="first-name">First Name * </label>
         <input
           class="form-input"
           type="text"
+          v-model="form.firstName"
           name="first-name"
           placeholder="John"
         />
@@ -23,6 +24,7 @@
         <input
           class="form-input"
           type="text"
+          v-model="form.lastName"
           name="last-name"
           placeholder="Doe"
         />
@@ -33,6 +35,7 @@
       <input
         class="form-input"
         type="email"
+        v-model="form.email"
         name="email"
         placeholder="example@protonmail.com"
       />
@@ -42,11 +45,43 @@
       <textarea
         name="message"
         class="form-input"
+        v-model="form.message"
         cols="30"
         rows="10"
         placeholder="Your message here"
       ></textarea>
     </div>
+    <div>
+      <label for="math"
+        >Anti-spam check: What's {{ form.firstNumber }} {{ form.operation }}
+        {{ form.secondNumber }} ? *</label
+      >
+      <input type="number" class="form-input mb-4" v-model="form.total" />
+    </div>
     <button type="submit" class="btn">Submit</button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form :{
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+        firstNumber: Math.floor(Math.random() * (9 - 2)) + 2,
+        secondNumber: Math.floor(Math.random() * (5 - 1)) + 1,
+        operation: Math.random() < 0.5 ? "+" : "-",
+        total: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit: async function() {
+      console.log(this.form)
+    }
+  }
+}
+</script>
