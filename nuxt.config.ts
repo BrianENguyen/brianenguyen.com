f97b04546f31efb201232b56f83be5bc2a66a62d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-23 17:24:22 -0500

    :lipstick: style: page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'fade', mode: 'out-in' },
+  },
   devtools: { enabled: true },
   modules: ['@unocss/nuxt', '@nuxt/content'],
   content: {
