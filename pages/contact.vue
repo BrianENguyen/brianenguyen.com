4c1681f20d2421193503ba013a4b7804db069401
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-22 23:08:49 -0500

    :sparkles: init: contact page

---
+++ pages/contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <UiJumbotron
+    jumbotronTitle="Contact"
+    src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1200,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg"
+    srcMobile="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600,b_black,o_70/v1639358227/BEN_Website/jumbotron/BEN_weekofwelcome_dtbkcj.jpg"
+  />
+  <DefaultLayout> </DefaultLayout>
+</template>
