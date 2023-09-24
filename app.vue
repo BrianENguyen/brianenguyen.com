2d99c1cfa2bb38d88a057b3e6233955b8e7370e0
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 11:42:17 -0500

    :lipstick: style: light and dark anchor tags

--- app.vue
+++ app.vue
@@ -8,12 +8,29 @@
 
 <style>
 html {
-  color: black;
+  color: #202020;
+}
+
+html a {
+  color: #0495b1;
+  transition: 0.3s color;
+}
+html a:hover {
+  color: #015768;
 }
 html.dark {
   color-scheme: dark;
   background-color: #1d1d1d;
   color: #cdd9e5;
+}
+
+
+html.dark a {
+  color: #79c2d0;
+  transition: 0.3s color;
+}
+html.dark a:hover {
+  color: #efefef;
 }
 
 .fade-enter-active,
