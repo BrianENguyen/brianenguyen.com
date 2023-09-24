efb57f010f8d480d184f89c3cfa58b9612645206
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 09:23:30 -0500

    :sparkles: feat: cd button

--- components/ui/cd.vue
+++ components/ui/cd.vue
@@ -1,3 +1,17 @@
 <template>
-  <NuxtLink to="/" class=""> cd .. </NuxtLink>
+  <NuxtLink
+    :to="to"
+    class="inline-block font-[monospace] text-xl decoration-none my-4
+        !text-[#9e9d9d] hover:!text-[#cdd9e5] cursor-pointer
+          transition-color duration-300"
+    title="This means to go back to the previous page (based on the site's hierarchy)"
+  >
+    cd ..
+  </NuxtLink>
 </template>
+
+<script>
+export default {
+  props: ['to']
+}
+</script>
