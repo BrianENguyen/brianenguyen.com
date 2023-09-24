cbf030dddd8d69478eedf9cd9bcbbebc5f61e605
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 16:53:51 -0500

    :sparkles: init: welcome modal

---
+++ components/ui/WelcomeModal.vue
@@ -0,0 +1,35 @@
+<script setup>
+const isVisible = useState('is-visible', () => true);
+
+function visibilityHandler() {
+  isVisible.value = false;
+  localStorage.setItem('modalHidden', '1');
+}
+
+onMounted(() => {
+  const item = localStorage.getItem('modalHidden');
+  if (!item) return;
+  if (item >= 3) {
+    isVisible.value = true;
+    localStorage.removeItem('modalHidden');
+    return;
+  }
+  isVisible.value = false;
+  let refreshCount = item;
+  refreshCount = +refreshCount + 1;
+  localStorage.setItem('modalHidden', refreshCount);
+})
+</script>
+
+<template>
+  <div
+    id="welcome-modal"
+    class="fixed gap-4 flex bottom-0 z-2 bg-white rounded p-4 dark:bg-[#131313] border-2 border-solid border-brian-blue"
+  >
+    <div>
+      <p>Welcome to my newly designed website, built with Nuxt3!</p>
+      <p>Blog detailing these changes coming soon...</p>
+    </div>
+    <span class="cursor-pointer text-2xl font-bold float-right">X</span>
+  </div>
+</template>
