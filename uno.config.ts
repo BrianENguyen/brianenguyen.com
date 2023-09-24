1aae2653acf4929a08bdc08a952d57fdd34b8a1a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 11:28:11 -0500

    :wrench: chore: light style configs

--- uno.config.ts
+++ uno.config.ts
@@ -12,14 +12,16 @@ export default defineConfig({
     },
     colors: {
       brianBlue: '#79c2d0',
+      brianBlueDark: '#0495b1',
       brianBlueLight: '#d4f3f9',
       grayLight: '#282c34',
+      black: '#202020',
     },
   },
   presets: [presetIcons({ prefix: 'i-' }), presetUno()],
   shortcuts: {
-    btn: 'cursor-pointer font-[Poppins] inline-block py-4 px-8 bg-transparent rounded border-solid border-1 !text-white decoration-none border-brian-blue transition duration-300 hover:bg-brian-blue font-700',
+    btn: 'cursor-pointer font-[Poppins] inline-block py-4 px-8 bg-transparent rounded border-solid border-1 text-black dark:!text-white decoration-none border-brian-blue transition duration-300 hover:bg-brian-blue font-700',
     'form-input':
-      'w-full mx-auto overflow-hidden border-none rounded outline-none bg-#323645 px-4 py-5 mt-4 text-white focus:border-2 focus:border-solid focus:border-brian-blue focus:bg-#494e63',
+      'w-full mx-auto overflow-hidden border-1 border-solid border-brian-blue-dark rounded outline-none dark:bg-#323645 px-4 py-5 mt-4 dark:text-white focus:border-2 focus:border-solid focus:border-brian-blue dark:focus:bg-#494e63',
   },
 });
