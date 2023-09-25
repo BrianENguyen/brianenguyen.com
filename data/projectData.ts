e4a1fe579afe63f030e7aec2df8afefd64c29cf9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 22:35:41 -0500

    :truck: chore: project data

---
+++ data/projectData.ts
@@ -0,0 +1,58 @@
+const projectData = [
+  {
+    title: 'My Site v3 - Nuxt3',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1694281377/BEN_Website/projects/BEN_languna_profile.webp',
+    alt: 'Me posing in front of Laguna Beach',
+    link: '/portfolio/my-site-v3',
+  },
+  {
+    title: 'All Elite Network',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1651693922/BEN_Website/projects/thunder_rosa.webp',
+    alt: "AEW wrestler Thunder Rosa holding the AEW Women's World Championship",
+    link: '/portfolio/all-elite-network',
+    photoCredit: {
+      name: 'All Elite Wrestling',
+      link: 'https://www.allelitewrestling.com/',
+    },
+  },
+  {
+    title: 'My Site v2 - React',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639358227/BEN_Website/projects/BEN_3098_islkyr.jpg',
+    alt: 'Me standing in front of the sunset at Redondo Beach',
+    link: '/portfolio/my-site-v2',
+  },
+  {
+    title: 'CSULB Tzu Ching',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639515139/BEN_Website/recent_projects/lbtc_group.jpg',
+    alt: 'CSULB Tzu Ching group photo',
+    link: '/portfolio/csulb-tzu-ching',
+  },
+  {
+    title: 'YelpCamp',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639529477/BEN_Website/projects/hugues-de-buyer_camp_fc2lzb.jpg',
+    alt: 'Camping tent set up underneath visible stars',
+    link: '/portfolio/yelpcamp',
+    photoCredit: {
+      name: 'Hugues de Buyer-Mimeure',
+      link: 'https://unsplash.com/@huguesdb',
+    },
+  },
+  {
+    title: 'First Personal Website',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639774676/BEN_Website/projects/ben_longexposure.jpg',
+    alt: 'Long exposure spiral effect using blue LED lights',
+    link: '/portfolio/first-personal-site',
+  },
+  {
+    title: 'Senior Project: Kraken Bracket',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_600/v1639530141/BEN_Website/projects/florian-olivo_gaming_im9ugb.jpg',
+    alt: 'Gamer playing a PC game',
+    link: '/portfolio/kraken-bracket',
+    photoCredit: {
+      name: 'Florian Olivo',
+      link: 'https://unsplash.com/@florianolv',
+    },
+  },
+];
+
+export default projectData;
