2cd682c74c2b938920dfd0bb93484aac8d7473fb
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-24 19:02:16 -0500

    :sparkles: feat: new commit date format

--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -10,16 +10,22 @@ fetch('https://api.github.com/repos/buraiyen/brianenguyen.com/branches/main')
     latestCommitDate.value = formatDate(retrievedDate)
   })
 
-function formatDate(dateStr) {
-  const date = new Date(dateStr);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+function formatDate(date) {
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  const dayOfWeek = daysOfWeek[date.getDay()];
   const day = String(date.getDate()).padStart(2, '0');
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+  return `${dayOfWeek}. ${day} ${month}, ${year} @ ${hours}:${minutes}:${seconds} (CT)`;
 }
 </script>
 
